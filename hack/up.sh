2cfeb87f30a53b3763f65c28dc1716d9eec4b8a2
Nick Mitchell <nickm@us.ibm.com>
2023-06-05 18:21:52 -0400

    fix: secrets injection was buggy

--- hack/up.sh
+++ hack/up.sh
@@ -9,6 +9,6 @@ wait
 "$SCRIPTDIR"/build.sh
 
 echo "$(tput setaf 2)Booting CodeFlare$(tput sgr0)"
-helm install $PLA platform $HELM_SECRETS && \
-    helm install $IBM ibm && \
+helm install $PLA platform && \
+    helm install $IBM ibm $HELM_SECRETS && \
     helm install $RUN tests/run
