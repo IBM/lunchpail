4a02013602af87e023dc75e9a189981e1b415bdf
Nick Mitchell <nickm@us.ibm.com>
2023-07-14 19:44:25 -0400

    fix: remove nope debugging output

--- platform/charts/core/kustomize.sh
+++ platform/charts/core/kustomize.sh
@@ -13,7 +13,6 @@ if [[ ${1-up} = up ]]; then
     $KUBECTL wait --for condition=established --timeout=60s crd/applications.app.k8s.io
     $KUBECTL apply -k $RSRC
 else
-    echo nope
     $KUBECTL delete -k $RSRC --ignore-not-found
     $KUBECTL delete -k $CRDS --ignore-not-found
 fi
