0aad0cbe8dd139a1a320cac3e6abf91474962e1d
Maria Camila Ruiz Cardenas <Maria.ruiz.cardenas@ibm.com>
2023-08-18 11:42:15 +0200

    feat: Add react router to client
    No change other than to serve the existing App.tsx via the `/` client-side route.

--- platform/dashboard/src/client/main.tsx
+++ platform/dashboard/src/client/main.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import { App } from "./App"
 
@@ -7,6 +8,10 @@ import "@patternfly/react-core/dist/styles/base.css"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
