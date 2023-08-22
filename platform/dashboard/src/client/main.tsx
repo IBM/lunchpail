9734ccb993be4e293fe6c6a45bfb9c98a18b9a61
Maria Camila Ruiz Cardenas <Maria.ruiz.cardenas@ibm.com>
2023-08-22 12:01:25 -0400

    fix: Added React Router error handler

--- platform/dashboard/src/client/main.tsx
+++ platform/dashboard/src/client/main.tsx
@@ -1,22 +1,33 @@
 import { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { createBrowserRouter, RouterProvider, useRouteError } from "react-router-dom"
 
 import { App } from "./App"
 import { DemoDataSetEventSource, DemoWorkerPoolEventSource } from "./events/demo"
 
 import "@patternfly/react-core/dist/styles/base.css"
 
+function ErrorBoundary() {
+  const error = useRouteError()
+  console.error(error)
+  return <div>Internal Error</div>
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App datasets="/datasets" workerpools="/workerpools" />,
+    errorElement: <ErrorBoundary />,
+  },
+  {
+    path: "/demo",
+    element: <App datasets={new DemoDataSetEventSource()} workerpools={new DemoWorkerPoolEventSource()} />,
+    errorElement: <ErrorBoundary />,
+  },
+])
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path={"/"} element={<App datasets="/datasets" workerpools="/workerpools" />} />
-        <Route
-          path={"/demo"}
-          element={<App datasets={new DemoDataSetEventSource()} workerpools={new DemoWorkerPoolEventSource()} />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
