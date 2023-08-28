8f00b165e54cf1b30fad5bf547360e1fcb972176
Nick Mitchell <nickm@us.ibm.com>
2023-08-28 11:15:13 -0400

    feat: show error message on error boundary page

--- platform/dashboard/src/client/components/ErrorBoundary.tsx
+++ platform/dashboard/src/client/components/ErrorBoundary.tsx
@@ -1,7 +1,29 @@
 import { useRouteError } from "react-router-dom"
+import { Bullseye, Stack, StackItem, Text, TextContent } from "@patternfly/react-core"
 
 export default function ErrorBoundary() {
   const error = useRouteError()
-  console.error(error)
-  return <div>Internal Error</div>
+  return (
+    <Bullseye>
+      <Stack hasGutter>
+        <StackItem isFilled />
+        <StackItem>
+          <Bullseye>
+            <img src="/assets/404.png" />
+          </Bullseye>
+        </StackItem>
+        <StackItem>
+          <Bullseye>
+            <TextContent>
+              <Text component="h1" style={{ textAlign: "center" }}>
+                Internal Error
+              </Text>
+              <Text component="p">{String(error)}</Text>
+            </TextContent>
+          </Bullseye>
+        </StackItem>
+        <StackItem isFilled />
+      </Stack>
+    </Bullseye>
+  )
 }
