179d620850ccbc95e59adf4b784938ecb815f929
Nick Mitchell <nickm@us.ibm.com>
2023-09-13 14:42:34 -0400

    feat: a bit more fleshing out of pages/NewWorkerPool

--- platform/dashboard/src/client/pages/NewWorkerPool.tsx
+++ platform/dashboard/src/client/pages/NewWorkerPool.tsx
@@ -1,7 +1,119 @@
-import Base from "./Base"
+import { ReactNode } from "react"
+import { Link } from "react-router-dom"
 
-export default class NewWorkerPool extends Base {
+import {
+  ActionGroup,
+  Button,
+  Form,
+  FormContextProvider,
+  FormContextProps,
+  FormGroup,
+  FormHelperText,
+  FormSection,
+  Grid,
+  GridItem,
+  HelperText,
+  HelperTextItem,
+  TextInput,
+  ToolbarItem,
+} from "@patternfly/react-core"
+
+import Base, { BaseState } from "./Base"
+
+import BackIcon from "@patternfly/react-icons/dist/esm/icons/arrow-left-icon"
+
+type Values = FormContextProps["values"]
+type SetValue = FormContextProps["setValue"]
+
+type State = BaseState & {
+  isCreating: boolean
+}
+
+export default class NewWorkerPool extends Base<unknown, State> {
+  private readonly handleNameChange = () => {}
+
+  private name(setValue: SetValue, values: Values) {
+    const name = values.poolName
+
+    return (
+      <FormGroup label="Pool name" isRequired fieldId="poolName">
+        <TextInput
+          isRequired
+          type="text"
+          id="poolName"
+          name="poolName"
+          aria-describedby="poolName-helper"
+          value={name}
+          onChange={(evt, value) => setValue("poolName", value)}
+        />
+        <FormHelperText>
+          <HelperText>
+            <HelperTextItem>Choose a name for your worker pool</HelperTextItem>
+          </HelperText>
+        </FormHelperText>
+      </FormGroup>
+    )
+  }
+
+  private doCreate(values: Values) {
+    console.log(values) // make eslint happy
+    this.setState({ isCreating: true })
+  }
+
+  private actions(values: Values) {
+    return (
+      <ActionGroup>
+        <Button isLoading={this.state?.isCreating} onClick={() => this.doCreate(values)}>
+          Create
+        </Button>
+
+        {this.returnToDashboardButton("secondary", "Cancel")}
+      </ActionGroup>
+    )
+  }
+
+  private returnToDashboardButton(
+    variant: "link" | "secondary" = "link",
+    text: ReactNode = (
+      <>
+        <BackIcon /> Return to Dashboard
+      </>
+    ),
+  ) {
+    const queryParams = new URLSearchParams(window.location.search)
+    const returnTo = queryParams.get("returnto") || "/"
+    return (
+      <Button
+        isInline
+        variant={variant}
+        component={(props) => (
+          <Link {...props} to={returnTo}>
+            {text}
+          </Link>
+        )}
+      />
+    )
+  }
+
+  protected override footerRight() {
+    return <ToolbarItem>{this.returnToDashboardButton()}</ToolbarItem>
+  }
+
   protected override body() {
-    return "hi"
+    return (
+      <FormContextProvider initialValues={{ poolName: "mypool" }}>
+        {({ setValue, values }) => (
+          <Form>
+            <FormSection title="Create Worker Pool">
+              <Grid hasGutter md={6}>
+                <GridItem span={12}>{this.name(setValue, values)}</GridItem>
+              </Grid>
+            </FormSection>
+
+            {this.actions(values)}
+          </Form>
+        )}
+      </FormContextProvider>
+    )
   }
 }
