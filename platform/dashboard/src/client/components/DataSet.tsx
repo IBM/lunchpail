32c0a0c2c2aca18e75e0367c9bc0ca0e663e05e4
Nick Mitchell <nickm@us.ibm.com>
2023-09-20 13:41:37 -0400

    chore: update DataSet to use CardsInGallery base class

--- platform/dashboard/src/client/components/DataSet.tsx
+++ platform/dashboard/src/client/components/DataSet.tsx
@@ -1,21 +1,8 @@
-import { PropsWithChildren, PureComponent } from "react"
-import {
-  Card,
-  CardBody,
-  CardHeader,
-  CardTitle,
-  Flex,
-  FlexItem,
-  DescriptionList,
-  DescriptionListTerm,
-  DescriptionListGroup,
-  DescriptionListDescription,
-  Stack,
-  StackItem,
-} from "@patternfly/react-core"
+import type { PropsWithChildren } from "react"
+import { Flex, FlexItem, Stack, StackItem } from "@patternfly/react-core"
 
 import Sparkline from "./Sparkline"
-import SmallLabel from "./SmallLabel"
+import CardInGallery from "./CardInGallery"
 import GridCell, { GridTypeData } from "./GridCell"
 
 import { meanCompletionRate, completionRateHistory } from "./CompletionRate"
@@ -34,40 +21,26 @@ type Props = Omit<DataSetModel, "timestamp"> &
     inboxHistory: number[]
   }
 
-function Work(
-  props: PropsWithChildren<Pick<Props, "idx"> & { label: string; count: number; history: Props["inboxHistory"] }>,
-) {
+function Work(props: PropsWithChildren<Pick<Props, "idx"> & { history: Props["inboxHistory"] }>) {
   return (
-    <DescriptionListGroup>
-      <DescriptionListTerm>
-        <SmallLabel count={props.count}>{props.label}</SmallLabel>
-      </DescriptionListTerm>
+    <Stack hasGutter>
+      <StackItem>
+        <Flex
+          className="codeflare--workqueue"
+          gap={{ default: "gapXs" }}
+          flexWrap={{ default: "nowrap" }}
+          style={{ width: "calc((4px + 1.375em) * 8 - 3px)", alignContent: "flex-start", overflow: "hidden" }}
+        >
+          {props.children}
+        </Flex>
+      </StackItem>
 
-      <DescriptionListDescription>
-        <Stack hasGutter>
-          <StackItem>
-            <Flex
-              className="codeflare--workqueue"
-              gap={{ default: "gapXs" }}
-              flexWrap={{ default: "nowrap" }}
-              style={{ width: "calc((4px + 1.375em) * 8 - 3px)", alignContent: "flex-start", overflow: "hidden" }}
-            >
-              {props.children}
-            </Flex>
-          </StackItem>
-
-          {props.history.length > 0 && (
-            <StackItem>
-              <Sparkline data={props.history} datasetIdx={props.idx} />
-            </StackItem>
-          )}
-        </Stack>
-      </DescriptionListDescription>
-    </DescriptionListGroup>
+      <StackItem>{props.history.length > 0 && <Sparkline data={props.history} datasetIdx={props.idx} />}</StackItem>
+    </Stack>
   )
 }
 
-export default class DataSet extends PureComponent<Props> {
+export default class DataSet extends CardInGallery<Props> {
   private stack(model: Props["inbox"] | Props["outbox"], gridDataType: GridTypeData) {
     if (!model) {
       return <GridCell type="placeholder" dataset={this.props.idx} />
@@ -82,42 +55,33 @@ export default class DataSet extends PureComponent<Props> {
       ))
   }
 
-  private unassigned() {
-    return this.stack(this.props.inbox, "unassigned")
-  }
-
   private outbox() {
     return <Sparkline data={completionRateHistory(this.props)} />
   }
 
-  public override render() {
-    return (
-      <Card isRounded>
-        <CardHeader>
-          <CardTitle>
-            <DataSetIcon /> {this.props.label}
-          </CardTitle>
-        </CardHeader>
-        <CardBody>
-          <DescriptionList isCompact>
-            <Work
-              label="Unassigned Work"
-              count={this.props.inbox}
-              idx={this.props.idx}
-              history={this.props.inboxHistory}
-            >
-              {this.unassigned()}
-            </Work>
+  protected override icon() {
+    return <DataSetIcon />
+  }
 
-            <DescriptionListGroup>
-              <DescriptionListTerm>
-                <SmallLabel count={meanCompletionRate(this.props)}>Completion Rate</SmallLabel>
-              </DescriptionListTerm>
-              <DescriptionListDescription>{this.outbox()}</DescriptionListDescription>
-            </DescriptionListGroup>
-          </DescriptionList>
-        </CardBody>
-      </Card>
+  protected override label() {
+    return this.props.label
+  }
+
+  private unassigned() {
+    return this.descriptionGroup(
+      "Unassigned Work",
+      <Work idx={this.props.idx} history={this.props.inboxHistory}>
+        {this.stack(this.props.inbox, "unassigned")}
+      </Work>,
+      this.props.inbox,
     )
+  }
+
+  private completions() {
+    return this.descriptionGroup("Completion Rate", this.outbox(), meanCompletionRate(this.props))
+  }
+
+  protected override groups() {
+    return [this.unassigned(), this.completions()]
   }
 }
