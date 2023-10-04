59f29769233577e26f4e1e3ed31704286c80c039
Nick Mitchell <nickm@us.ibm.com>
2023-10-04 10:50:45 -0400

    fix: Grid stacks are sometimes blank (7, 14, 28, 29, ...)
    The fix was to avoid some floating point rounding error propagation by deferring the division to the end.
    
    Fixes #393

--- platform/dashboard/src/client/components/Queue.tsx
+++ platform/dashboard/src/client/components/Queue.tsx
@@ -14,6 +14,12 @@ export type Props = {
 }
 
 export default class Queue extends PureComponent<Props> {
+  // change-maker works in terms of dollar currency, but accepts coins
+  // in terms of cents. To avoid propagation of rounding errors, we
+  // convert our cents to dollars here (*100), and then do a /100 at the very
+  // end -- i.e. in this way we get only a single rounding error.
+  private readonly coinDenominations: number[] = [1, 10, 100, 1000].map((_) => _ * 100)
+
   /** Render one cell */
   private cell(cellType: GridTypeData, dataset: string, labelNum: number, stackDepth: number) {
     return (
@@ -30,15 +36,13 @@ export default class Queue extends PureComponent<Props> {
     return Object.entries(tasks || {})
       .filter(([, size]) => size > 0)
       .flatMap(([dataset, size]) => {
-        /**
-         * gridCellStacking() returns a mapping from coin denomination to a the first
-         * parameter, 'value'. Currently, gridCellStacking() requires that 'value' be
-         * a number representing US cents, so we hard-code the '$' below and divide
-         * the number 'size' by 100 to get number representing the depth of a stack.
-         */
-        const coinDenominations: number[] = [1, 10, 100, 1000]
+        // gridCellStacking() returns a mapping from coin denomination
+        // the number of such coins ('value'). Currently,
+        // gridCellStacking() requires that the first paramter be a
+        // currency, so we add the '$' prefix
+        //
         return (
-          Object.entries(gridCellStacking("$" + size / 100, coinDenominations))
+          Object.entries(gridCellStacking("$" + size, this.coinDenominations))
             // Find the number of stacks that are being used to render 'size' GridCells by finding the non-zero values from gridCellStacking()
             .filter(([, numStacks]) => numStacks > 0)
             .map(([stackDepth, numStacks]) =>
@@ -46,7 +50,7 @@ export default class Queue extends PureComponent<Props> {
               // TODO: denote a stack changed depth via darker color change in the next PR
               Array(numStacks)
                 .fill(0)
-                .map((_, idx) => this.cell(cellType, dataset, idx, parseInt(stackDepth, 10))),
+                .map((_, idx) => this.cell(cellType, dataset, idx, parseInt(stackDepth, 10) / 100)),
             )
         )
       })
