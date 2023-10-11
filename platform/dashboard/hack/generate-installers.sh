406cd352e7f370248b2aae4717276257d9fb4004
Nick Mitchell <nickm@us.ibm.com>
2023-10-11 16:10:48 -0400

    fix: add missing CRDs to pre-compiled platform yamls

--- platform/dashboard/hack/generate-installers.sh
+++ platform/dashboard/hack/generate-installers.sh
@@ -4,13 +4,13 @@ set -e
 set -o pipefail
 
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
-OUTDIR="$SCRIPTDIR"/../deploy
+OUTDIR="$SCRIPTDIR"/../resources
 TOP="$SCRIPTDIR"/../../..
 
 if [[ ! -e "$OUTDIR" ]]
 then mkdir "$OUTDIR"
 fi
 
-helm install --dry-run codeflare-platform "$TOP"/platform --set nvidia.enabled=false --set ray.enabled=false --set kube-fledged.enabled=false --set spark.enabled=false > "$OUTDIR"/jaas-lite.yml
+helm template --include-crds codeflare-platform "$TOP"/platform --set nvidia.enabled=false --set ray.enabled=false --set kube-fledged.enabled=false --set spark.enabled=false > "$OUTDIR"/jaas-lite.yml
 
-helm install --dry-run codeflare-platform "$TOP"/platform --set nvidia.enabled=false --set ray.enabled=true --set kube-fledged.enabled=true --set spark.enabled=true > "$OUTDIR"/jaas-full.yml
+helm template --include-crds codeflare-platform "$TOP"/platform --set nvidia.enabled=false --set ray.enabled=true --set kube-fledged.enabled=true --set spark.enabled=true > "$OUTDIR"/jaas-full.yml
