0957bd41bf5ebb09248f5c13cac44c62be2e4ad2
Nick Mitchell <nickm@us.ibm.com>
2023-10-20 11:49:13 -0400

    fix: add managed-by and component labels to defaults and examples
    Also removes pre-generated resource specs from the repo, in favor of generating them on the fly.

--- hack/travis/setup.sh
+++ hack/travis/setup.sh
@@ -13,3 +13,12 @@ AI_FOUNDATION_GITHUB_PAT=$AI_FOUNDATION_GITHUB_PAT
 COS_ACCESS_KEY=$COS_ACCESS_KEY
 COS_SECRET_KEY=$COS_SECRET_KEY
 EOF
+
+if ! which helm >& /dev/null
+then
+    echo "$(tput setaf 2)Installing helm$(tput sgr0)"
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+    chmod 700 get_helm.sh
+    ./get_helm.sh
+    rm get_helm.sh
+fi
