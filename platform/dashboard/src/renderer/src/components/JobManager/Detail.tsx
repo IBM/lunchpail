4b02d8e7b840b446860444e562ea4c86e524a819
Nick Mitchell <nickm@us.ibm.com>
2023-10-22 19:09:24 -0400

    chore: update JobManager/Detail to use `useContext`
    this simplifies the syntax a bit

--- platform/dashboard/src/renderer/src/components/JobManager/Detail.tsx
+++ platform/dashboard/src/renderer/src/components/JobManager/Detail.tsx
@@ -1,3 +1,4 @@
+import { useContext } from "react"
 import { Button, Tooltip } from "@patternfly/react-core"
 
 import Status from "../../Status"
@@ -15,61 +16,54 @@ import SyncIcon from "@patternfly/react-icons/dist/esm/icons/sync-icon"
 import TrashIcon from "@patternfly/react-icons/dist/esm/icons/trash-icon"
 
 export default function JobManagerDetail() {
-  return (
-    <Settings.Consumer>
-      {(settings) => (
-        <Status.Consumer>
-          {(status) => {
-            const demoMode = settings?.demoMode[0] ?? false
+  const status = useContext(Status)
+  const settings = useContext(Settings)
 
-            const rest =
-              demoMode || !status.status
-                ? []
-                : Object.entries(status.status).map(([key, value]) =>
-                    descriptionGroup(camelCaseSplit(key), value, undefined, descriptions[key]),
-                  )
+  const demoMode = settings?.demoMode[0] ?? false
 
-            const init = () => status.setTo("updating")
-            const destroy = () => status.setTo("destroying")
+  const rest =
+    demoMode || !status.status
+      ? []
+      : Object.entries(status.status).map(([key, value]) =>
+          descriptionGroup(camelCaseSplit(key), value, undefined, descriptions[key]),
+        )
 
-            const initButtonIsLoading = status.refreshing === "initializing" || status.refreshing === "updating"
-            const destroyButtonIsLoading = status.refreshing === "destroying"
+  const init = () => status.setTo("updating")
+  const destroy = () => status.setTo("destroying")
 
-            const actions =
-              status.status && isHealthy(status.status)
-                ? [
-                    <Tooltip key="refresh" content="Reload the Job Manager with the initial configuration">
-                      <Button
-                        size="sm"
-                        variant="secondary"
-                        onClick={init}
-                        isLoading={initButtonIsLoading}
-                        icon={initButtonIsLoading ? <></> : <SyncIcon />}
-                      >
-                        {status.refreshing === "updating" ? "Refreshing" : "Refresh"}
-                      </Button>
-                    </Tooltip>,
+  const initButtonIsLoading = status.refreshing === "initializing" || status.refreshing === "updating"
+  const destroyButtonIsLoading = status.refreshing === "destroying"
 
-                    <Tooltip key="delete" content="Deprovision the Job Manager">
-                      <Button
-                        size="sm"
-                        variant="danger"
-                        onClick={destroy}
-                        isLoading={destroyButtonIsLoading}
-                        icon={destroyButtonIsLoading ? <></> : <TrashIcon />}
-                      >
-                        {status.refreshing === "destroying" ? "Deleting" : "Delete"}
-                      </Button>
-                    </Tooltip>,
-                  ]
-                : undefined
+  const actions =
+    status.status && isHealthy(status.status)
+      ? [
+          <Tooltip key="refresh" content="Reload the Job Manager with the initial configuration">
+            <Button
+              size="sm"
+              variant="secondary"
+              onClick={init}
+              isLoading={initButtonIsLoading}
+              icon={initButtonIsLoading ? <></> : <SyncIcon />}
+            >
+              {status.refreshing === "updating" ? "Refreshing" : "Refresh"}
+            </Button>
+          </Tooltip>,
 
-            const body = dl([...summaryGroups(demoMode, status.status), ...rest])
+          <Tooltip key="delete" content="Deprovision the Job Manager">
+            <Button
+              size="sm"
+              variant="danger"
+              onClick={destroy}
+              isLoading={destroyButtonIsLoading}
+              icon={destroyButtonIsLoading ? <></> : <TrashIcon />}
+            >
+              {status.refreshing === "destroying" ? "Deleting" : "Delete"}
+            </Button>
+          </Tooltip>,
+        ]
+      : undefined
 
-            return <DrawerContent body={body} actions={actions} />
-          }}
-        </Status.Consumer>
-      )}
-    </Settings.Consumer>
-  )
+  const body = dl([...summaryGroups(demoMode, status.status), ...rest])
+
+  return <DrawerContent body={body} actions={actions} />
 }
