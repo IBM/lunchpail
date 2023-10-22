645ed6b46d9c3f5334ab34de286d0cbfca0139d4
Nick Mitchell <nickm@us.ibm.com>
2023-10-22 18:40:58 -0400

    fix: shorten resource descriptions to prevent initial line wrap discrepancies

--- platform/dashboard/src/renderer/src/names.tsx
+++ platform/dashboard/src/renderer/src/names.tsx
@@ -42,16 +42,14 @@ export const subtitles: Record<NavigableKind, import("react").ReactNode> = {
   ),
   applications: (
     <span>
-      The registered code bases in your system. Each <strong>Application</strong> has a base image, a code repository,
-      and some configuration defaults. An Application may define one or more compatible{" "}
-      <Link to={hash("datasets")}>Task Queues</Link>.
+      Each <strong>Application</strong> has a base image, a code repository, and some configuration defaults. Each may
+      define one or more compatible <Link to={hash("datasets")}>Task Queues</Link>.
     </span>
   ),
   datasets: (
     <span>
-      The registered queues in your system. Each <strong>Task Queue</strong> is registered to be compatible with one or
-      more <Link to={hash("applications")}>Applications</Link>, and is linked to a data store as the place to queue up
-      the to-do tasks.
+      Each <strong>Task Queue</strong> is compatible with one or more{" "}
+      <Link to={hash("applications")}>Applications</Link>, and is linked to a place to queue up the to-do tasks.
     </span>
   ),
   workerpools: (
