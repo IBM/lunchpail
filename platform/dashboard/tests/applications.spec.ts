85ee66cb42568422dee8a8a6374d88460b2ca001
Maria Camila Ruiz Cardenas <maria.ruiz.cardenas@ibm.com>
2023-11-01 15:59:36 -0400

    test: checking that 4 demo mode app cards are visible

---
+++ platform/dashboard/tests/applications.spec.ts
@@ -0,0 +1,34 @@
+// @ts-check
+import { expect, test } from "@playwright/test"
+import launchElectron from "./launch-electron"
+
+test("4 applications visible when in demo mode", async () => {
+  // Launch Electron app.
+  const electronApp = await launchElectron()
+
+  // Get the first page that the app opens, wait if necessary.
+  const page = await electronApp.firstWindow()
+
+  // Check if we are in demo mode (should be true by default)
+  const demoModeStatus = await page.getByLabel("Demo Mode").isChecked()
+  console.log(`Demo mode on?: ${demoModeStatus}`)
+
+  // If in demo mode, then continue with Applications card test
+  if (demoModeStatus) {
+    // Get Applications tab element from the sidebar and click
+    await page.getByRole("link", { name: "Applications" }).click()
+
+    // Verify that the four showing are the salamander, pig, grasshopper, and worm cards
+    const expectedCards = ["salamander", "pig", "grasshopper", "worm"]
+
+    await Promise.all(
+      expectedCards.map((id) =>
+        expect(page.locator(`[data-ouia-component-type="PF5/Card"][data-ouia-component-id="${id}"]`))
+          .toBeVisible({
+            timeout: 30000,
+          })
+          .then(() => console.log("got", id)),
+      ),
+    )
+  }
+})
