fb1389e69f22a551c863d6e614577b49271fbe9b
Nick Mitchell <nickm@us.ibm.com>
2023-11-09 08:21:24 -0500

    fix: update workerpool/description to elaborate a bit more

--- platform/dashboard/src/renderer/src/content/workerpools/description.tsx
+++ platform/dashboard/src/renderer/src/content/workerpools/description.tsx
@@ -2,16 +2,14 @@ import { Link } from "react-router-dom"
 import { hash } from "@jay/renderer/navigate/kind"
 
 import { singular } from "./name"
-import { singular as taskqueuesSingular } from "../taskqueues/name"
 import { singular as applicationSingular } from "../applications/name"
 
 export default (
   <span>
-    Each <strong>{singular}</strong> is a set of workers that can process tasks from a{" "}
-    <strong>{taskqueuesSingular}</strong> using code from given{" "}
+    Each <strong>{singular}</strong> is a set of workers specialized to process tasks using code from a given set of{" "}
     <Link to={hash("applications")}>
       <strong>{applicationSingular}</strong>
     </Link>
-    .
+    . You may allocate more than one {singular} to a given task, and can bring them up and tear them down on the fly.
   </span>
 )
