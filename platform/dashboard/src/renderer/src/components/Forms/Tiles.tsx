83994c004af458501400ec3a63383b94d1b22562
Nick Mitchell <nickm@us.ibm.com>
2023-11-21 11:21:25 -0500

    chore: Forms/Tiles should use Gallery

--- platform/dashboard/src/renderer/src/components/Forms/Tiles.tsx
+++ platform/dashboard/src/renderer/src/components/Forms/Tiles.tsx
@@ -1,16 +1,13 @@
 import { type MouseEvent, type ReactNode, useCallback } from "react"
 
 import type NonEmptyArray from "@jay/util/NonEmptyArray"
-import { Flex, Tile, type TileProps } from "@patternfly/react-core"
+import { Gallery, Tile, type TileProps } from "@patternfly/react-core"
 
 import type { Ctrl, FormProps } from "./Props"
 
 export type TileOptions = NonEmptyArray<
   Pick<TileProps, "title" | "icon" | "isDisabled"> & { description: ReactNode; value?: string }
 >
-
-const flex1 = { flex: 1 }
-const flex1pf = { default: "flex_1" as const }
 
 export function Tiles(props: FormProps & Ctrl & { options: TileOptions }) {
   //const [selected, setSelected] = useState(props.ctrl.values[props.fieldId] ?? props.options[0].value ?? props.options[0].title)
@@ -29,25 +26,22 @@ export function Tiles(props: FormProps & Ctrl & { options: TileOptions }) {
 
   return (
     <div role="listbox" aria-label="Form Tiles">
-      <Flex>
+      <Gallery hasGutter>
         {props.options.map((tile) => (
-          <Flex key={tile.title} flex={flex1pf}>
-            <Tile
-              style={flex1}
-              data-value={tile.value ?? tile.title}
-              isDisplayLarge
-              isSelected={selected === tile.value ?? tile.title}
-              onClick={onClick}
-              isStacked
-              title={tile.title}
-              icon={tile.icon}
-              isDisabled={tile.isDisabled}
-            >
-              {tile.description}
-            </Tile>
-          </Flex>
+          <Tile
+            data-value={tile.value ?? tile.title}
+            isDisplayLarge
+            isSelected={selected === tile.value ?? tile.title}
+            onClick={onClick}
+            isStacked
+            title={tile.title}
+            icon={tile.icon}
+            isDisabled={tile.isDisabled}
+          >
+            {tile.description}
+          </Tile>
         ))}
-      </Flex>
+      </Gallery>
     </div>
   )
 }
