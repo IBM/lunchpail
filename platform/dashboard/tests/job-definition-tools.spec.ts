7370363ee4e6495a2bac0dc5b15fa3c67dab2576
Maria Camila Ruiz Cardenas <maria.ruiz.cardenas@ibm.com>
2023-11-29 09:27:39 -0500

    test: completed test for delete button

--- platform/dashboard/tests/job-definition-tools.spec.ts
+++ platform/dashboard/tests/job-definition-tools.spec.ts
@@ -1,42 +1,48 @@
 // @ts-check
-import { ElectronApplication, Locator, Page, expect, test } from "@playwright/test"
+import { Page, expect, test } from "@playwright/test"
 import launchElectron from "./launch-electron"
-import { navigateToCard } from "./navigate-to-queue-tab"
 import expectedApplications from "./applications"
-
+import { navigateToCard } from "./navigate-to-queue-tab"
 import { name } from "../src/renderer/src/content/applications/name"
 
 test.describe.serial("job definition tools running sequentially", () => {
-  let electronApp: ElectronApplication
   let page: Page
-  let demoModeStatus: boolean
-  let drawer: Locator
 
   const { application: expectedApp } = expectedApplications[0]
 
-  test("Navigate to drawer for a given job definition card", async () => {
+  test("Activate 'Job Definitions' tab", async () => {
     // Launch Electron app.
-    electronApp = await launchElectron()
+    const electronApp = await launchElectron()
 
     // Get the first page that the app opens, wait if necessary.
     page = await electronApp.firstWindow()
 
     // Check if we are in demo mode (should be true by default)
-    demoModeStatus = await page.getByLabel("Demo").isChecked()
+    const demoModeStatus = await page.getByLabel("Demo").isChecked()
     console.log(`Demo mode on?: ${demoModeStatus}`)
 
-    // get Applications tab element from the sidebar and click to activate Application gallery
+    // get 'Job Definitions' tab element from the sidebar and click to activate Job Definitions gallery
     await page.locator('[data-ouia-component-type="PF5/NavItem"]', { hasText: name }).click()
-
-    drawer = await navigateToCard(page, expectedApp)
   })
 
   test("Trash button opens 'Confirm Delete' modal", async () => {
+    // navigate to a given job definition card's drawer
+    const drawer = await navigateToCard(page, expectedApp)
+
     // click on trash button
     await drawer.locator(`[data-ouia-component-id="trashButton"]`).click()
 
     // check that deletion modal opened
     const modal = await page.locator(`[data-ouia-component-type="PF5/ModalContent"]`)
     await expect(modal).toBeVisible()
+  })
+
+  test("Confirm job definition deletion", async () => {
+    // click on Confirm button
+    await page.getByRole("button", { name: "Confirm" }).click()
+
+    // verify that intended job definition was deleted
+    const appCard = page.locator(`[data-ouia-component-type="PF5/Card"][data-ouia-component-id="${expectedApp}"]`)
+    await expect(appCard).toHaveCount(0)
   })
 })
