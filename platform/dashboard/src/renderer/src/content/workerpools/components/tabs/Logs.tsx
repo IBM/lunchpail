3c3d86e01b1ff8d04bd1c4d8960d0000eb35f2f5
Nick Mitchell <nickm@us.ibm.com>
2023-12-03 13:47:22 -0500

    fix: for now, only should workerpool application logs, not internal logs
    at some point, we can add a UI to allow the user to choose to see internal logs

--- platform/dashboard/src/renderer/src/content/workerpools/components/tabs/Logs.tsx
+++ platform/dashboard/src/renderer/src/content/workerpools/components/tabs/Logs.tsx
@@ -7,7 +7,7 @@ export default function WorkerPoolLogsTab(props: Props) {
   return LogsTab({
     selector: [
       `app.kubernetes.io/component=workerpool,app.kubernetes.io/name=${props.model.label}`,
-      `app.kubernetes.io/component=workstealer,app.kubernetes.io/name=${props.model.application}`,
+      // `app.kubernetes.io/component=workstealer,app.kubernetes.io/name=${props.model.application}`,
     ].join(":"),
     namespace: props.model.namespace,
   })
