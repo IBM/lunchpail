f986ca40a85e70ac865278ec652a0d2eb5a15227
Nick Mitchell <nickm@us.ibm.com>
2023-12-09 14:04:10 -0500

    fix: improve layout of Drawer toolbar to right-align everything except history

--- platform/dashboard/src/renderer/src/components/Drawer/Actions.tsx
+++ platform/dashboard/src/renderer/src/components/Drawer/Actions.tsx
@@ -1,15 +1,13 @@
 import { type ReactElement } from "react"
-import { ToolbarContent, ToolbarGroup, ToolbarItem, type ToolbarGroupProps } from "@patternfly/react-core"
+import { ToolbarGroup, ToolbarItem, type ToolbarGroupProps } from "@patternfly/react-core"
 
 /** Action buttons to be rendered in the Drawer footer */
 export default function Actions(props: Pick<ToolbarGroupProps, "variant"> & { children: ReactElement[] }) {
   return (
-    <ToolbarContent>
-      <ToolbarGroup variant={props.variant}>
-        {props.children.map((action) => (
-          <ToolbarItem key={action.key}>{action}</ToolbarItem>
-        ))}
-      </ToolbarGroup>
-    </ToolbarContent>
+    <ToolbarGroup variant={props.variant}>
+      {props.children.map((action) => (
+        <ToolbarItem key={action.key}>{action}</ToolbarItem>
+      ))}
+    </ToolbarGroup>
   )
 }
--- platform/dashboard/src/renderer/src/components/Drawer/Toolbar.tsx
+++ platform/dashboard/src/renderer/src/components/Drawer/Toolbar.tsx
@@ -1,28 +1,27 @@
 import type { ReactElement } from "react"
-import { Flex, FlexItem, Toolbar } from "@patternfly/react-core"
+import { Toolbar, ToolbarContent, ToolbarGroup } from "@patternfly/react-core"
 
 import Actions from "./Actions"
 import HistoryActions from "./History"
 
-const flex_1 = { default: "flex_1" as const }
+const flex_1 = { flex: 1 }
+
+function Filler() {
+  return <ToolbarGroup style={flex_1} />
+}
 
 /** Content to be shown as a footer toolbar inside the "sidebar" drawer */
 export default function DrawerToolbar(props: { actions?: ReactElement[]; rightActions?: ReactElement[] }) {
   return (
     <Toolbar>
-      <Flex>
-        <FlexItem>
-          <HistoryActions />
-        </FlexItem>
+      <ToolbarContent>
+        <HistoryActions />
+        <Filler />
 
-        <FlexItem flex={flex_1}>{props.actions && <Actions variant="button-group">{props.actions}</Actions>}</FlexItem>
+        {props.actions && <Actions variant="button-group">{props.actions}</Actions>}
 
-        {props.rightActions && (
-          <FlexItem>
-            <Actions variant="icon-button-group">{props.rightActions}</Actions>
-          </FlexItem>
-        )}
-      </Flex>
+        {props.rightActions && <Actions variant="icon-button-group">{props.rightActions}</Actions>}
+      </ToolbarContent>
     </Toolbar>
   )
 }
