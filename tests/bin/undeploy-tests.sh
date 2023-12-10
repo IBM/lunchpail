a5b63c73675bf891059e0cecd3bb33f406f440cd
Nick Mitchell <nickm@us.ibm.com>
2023-12-10 16:50:34 -0500

    test: improve reporting of stragglers on helm delete failure

--- tests/bin/undeploy-tests.sh
+++ tests/bin/undeploy-tests.sh
@@ -6,18 +6,23 @@ set -o pipefail
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 . "$SCRIPTDIR"/../../hack/settings.sh
 
-echo "$(tput setaf 2)Uninstalling test Runs for arch=$ARCH $1$(tput sgr0)"
-$HELM delete --ignore-not-found codeflare-tests --wait
-
 # in travis this can help us see whether there are straggler
 # namespaces, etc.
-echo "Checking for stragglers"
-$KUBECTL get ns
-$KUBECTL get application -A
-$KUBECTL get workerpools -A
-$KUBECTL get workdispatchers -A
-$KUBECTL get datasets -A
-echo "Done checking for stragglers"
+function report_stragglers {
+    echo "Checking for stragglers"
+    $KUBECTL get ns
+    $KUBECTL get application -A
+    $KUBECTL get workerpools -A
+    $KUBECTL get workdispatchers -A
+    $KUBECTL get datasets -A
+    echo "Done checking for stragglers"
+
+    # since we are only here if there was a failure
+    exit 1
+}
+
+echo "$(tput setaf 2)Uninstalling test Runs for arch=$ARCH $1$(tput sgr0)"
+$HELM delete --ignore-not-found codeflare-tests --wait || report_stragglers
 
 if [[ -n "$RUNNING_CODEFLARE_TESTS" ]]
 then
