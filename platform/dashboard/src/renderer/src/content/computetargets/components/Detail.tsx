4c2ba6b962806efb71905f94194b0f513aea6c12
Nick Mitchell <nickm@us.ibm.com>
2024-01-10 16:36:54 -0500

    fix: improve enable/disable UI for ComputeTarget

--- platform/dashboard/src/renderer/src/content/computetargets/components/Detail.tsx
+++ platform/dashboard/src/renderer/src/content/computetargets/components/Detail.tsx
@@ -1,15 +1,18 @@
 import { useContext, useCallback } from "react"
-import { Switch } from "@patternfly/react-core"
+import { Spinner, Switch, Tooltip } from "@patternfly/react-core"
 
 import Status from "@jay/renderer/Status"
 import Settings from "@jay/renderer/Settings"
-
-import { summaryGroups } from "./Card"
+import camelCaseSplit from "@jay/renderer/util/camel-split"
 import DrawerContent from "@jay/components/Drawer/Content"
 
-import camelCaseSplit from "@jay/renderer/util/camel-split"
+import { summaryGroups } from "./Card"
+
 import { dl as DescriptionList, descriptionGroup } from "@jay/components/DescriptionGroup"
 import { descriptions } from "@jay/common/status/JobManagerStatus"
+
+import { singular as computetarget } from "@jay/renderer/content/computetargets/name"
+import { name as computepools, singular as computepool } from "@jay/renderer/content/workerpools/name"
 
 import type Props from "./Props"
 
@@ -32,22 +35,42 @@ export default function JobManagerDetail(props: Props) {
   )
   // const destroyButtonIsLoading = status.refreshing === "destroying"
 
+  const tooltip =
+    status.refreshing === "destroying"
+      ? `Removing JaaS support from this ${computetarget}`
+      : status.refreshing === "initializing"
+        ? `Installing JaaS support on this ${computetarget}`
+        : props.spec.isJaaSWorkerHost
+          ? `This ${computetarget} is enabled to run ${computepools} Workers. Click to remove.`
+          : `If you wish to run ${computepool} Workers on this ${computetarget}, click to add this capability.`
+
   const rightActions = [
-    <Switch
-      key="jaas-toggler"
-      hasCheckIcon
-      onClick={toggle}
-      isDisabled={status.refreshing !== null}
-      data-ouia-component-id="comptueTargetEnableSwitch"
-      isChecked={props.spec.isJaaSWorkerHost}
-      label={
-        status.refreshing === "destroying"
-          ? "Deprovisioning"
-          : status.refreshing === "initializing"
-            ? "Initializing"
-            : "JaaS Enabled"
-      }
-    />,
+    <Tooltip key="jaas-toggler" position="left-end" content={tooltip}>
+      <Switch
+        hasCheckIcon
+        isReversed
+        onClick={toggle}
+        isDisabled={status.refreshing !== null}
+        data-ouia-component-id="comptueTargetEnableSwitch"
+        isChecked={props.spec.isJaaSWorkerHost}
+        label={
+          status.refreshing === "destroying" ? (
+            <>
+              <Spinner size="sm" />
+              Deprovisioning
+            </>
+          ) : status.refreshing === "initializing" ? (
+            <>
+              <Spinner size="sm" />
+              Initializing
+            </>
+          ) : (
+            "Enabled for JaaS Workers"
+          )
+        }
+        labelOff="Not enabled for JaaS Workers"
+      />
+    </Tooltip>,
   ]
 
   const summary = <DescriptionList groups={[...summaryGroups(demoMode, status.status, props), ...rest]} />
