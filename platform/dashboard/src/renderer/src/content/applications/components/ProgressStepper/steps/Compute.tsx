afaa540268c06a0fd72a84676fce316371925b55
Nick Mitchell <nickm@us.ibm.com>
2024-01-25 10:44:01 -0500

    fix: improve Compute ProgressStep popover content to always allow creation of new pool

--- platform/dashboard/src/renderer/src/content/applications/components/ProgressStepper/steps/Compute.tsx
+++ platform/dashboard/src/renderer/src/content/applications/components/ProgressStepper/steps/Compute.tsx
@@ -1,10 +1,11 @@
+import { Stack } from "@patternfly/react-core"
 import { LinkToNewPool } from "@jay/renderer/navigate/newpool"
 import { linkToAllDetails } from "@jay/renderer/navigate/details"
 
 import taskqueueProps from "../../taskqueueProps"
 
-import { singular as workerpool } from "@jay/resources/workerpools/name"
 import { groupSingular as application } from "@jay/resources/applications/group"
+import { name as workerpools, singular as workerpool } from "@jay/resources/workerpools/name"
 
 import type Step from "../Step"
 import type Props from "../../Props"
@@ -12,6 +13,25 @@ import { oopsNoQueue } from "../oops"
 
 function associatedWorkerPools(props: Props) {
   return props.workerpools.filter((_) => _.spec.application.name === props.application.metadata.name)
+}
+
+function noWorkers() {
+  return (
+    <span>
+      No workers have been assigned to this <strong>{application}</strong>. Once you configure a{" "}
+      <strong>Compute {workerpool}</strong>, the workers will begin to process any queued-up Tasks.
+    </span>
+  )
+}
+
+function numWorkers(nPools: number, nWorkers: number) {
+  return (
+    <span>
+      {nWorkers} <strong>{nWorkers === 1 ? "Worker" : "Workers"}</strong> spread across {nPools}{" "}
+      <strong>Compute {nPools === 1 ? workerpool : workerpools}</strong> {nWorkers === 1 ? "has" : "have"} been assigned
+      to this <strong>{application}</strong>.
+    </span>
+  )
 }
 
 const step: Step = {
@@ -23,17 +43,20 @@ const step: Step = {
 
     if (!queue) {
       return oopsNoQueue
-    } else if (pools.length === 0) {
-      const body = (
-        <span>
-          No workers have been assigned to this <strong>{application}</strong>. Once you configure a{" "}
-          <strong>Compute {workerpool}</strong>, the workers will begin to process any queued-up Tasks.
-        </span>
-      )
+    } else {
+      const nPools = pools.length
+      const nWorkers = pools.reduce((N, pool) => N + pool.spec.workers.count, 0)
+      const body =
+        pools.length === 0 ? (
+          noWorkers()
+        ) : (
+          <Stack hasGutter>
+            {numWorkers(nPools, nWorkers)}
+            {linkToAllDetails("workerpools", pools, undefined, onClick)}
+          </Stack>
+        )
       const footer = <LinkToNewPool isInline taskqueue={queue.name} startOrAdd="create" onClick={onClick} />
       return { body, footer }
-    } else {
-      return linkToAllDetails("workerpools", pools, undefined, onClick)
     }
   },
 }
