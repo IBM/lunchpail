0790ee74b9aebcced80080ab9d33f8792dfdd110
Nick Mitchell <nickm@us.ibm.com>
2024-02-16 12:15:03 -0500

    refactor: move source out of openroad app.yaml and into separate .py file
    plus remove some unnecessary `codeflare.dev/created-by: user` labels in that template

---
+++ platform/examples/src/openroad/max-utilization/main.py
@@ -0,0 +1,55 @@
+#
+# Origin:
+# https://github.com/ieee-ceda-datc/aspdac-2022-tutorial/blob/main/part4-k8s-single-node-ray-demo/k8s-single-node-ray.ipynb
+# "In this demo, our goal is to find a maximum achivable utilization for a given design."
+#
+import sys
+import subprocess
+from os import path, remove
+from shutil import move
+
+in_file = sys.argv[1]
+processing_file = sys.argv[2]
+out_file = sys.argv[3]
+
+print(f"in_file={in_file}")
+
+with open(in_file) as f:
+    util = int(f.readline())
+
+print(f"FP util={util}")
+
+# 0. mark work as underway
+move(in_file, processing_file)
+
+# 1. Copy experiment template
+mountpath = "/mnt/datasets/openroad-max-util-templates/"
+openroad_flow = path.join(mountpath, "openroad-flow")
+openroad_flow_local = path.join("/tmp", "openroad-flow")
+template = path.join(mountpath, "experiment-template")
+workspace = path.join("/tmp", f"experiment-{util}")
+subprocess.call(f"cp -r {template} {workspace}", shell=True)
+
+if not path.isdir(openroad_flow_local):
+    print("Copying openroad-flow")
+    subprocess.call(f"cp -r {openroad_flow} {openroad_flow_local}", shell=True)
+
+# 2. Change the utilization.
+with open(f"{workspace}/config.mk", 'a') as f:
+    f.write("\n# Experiment setup:\n")
+    f.write("export CORE_UTILIZATION = {}\n".format(util))
+
+# 3. Execute the flow
+print(f"Running experiment {util}")
+subprocess.call(f"make DESIGN_CONFIG=./config.mk",
+                cwd=workspace,
+                shell=True)
+
+subprocess.call(f"cp -r {workspace} {mountpath}", shell=True)
+
+# 4. mark work as done
+remove(processing_file)
+
+# 5. TODO register output in out_file
+
+print(f"FP util {util} -- Done.")
