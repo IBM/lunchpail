e4cf370dbfdb9011be2fa1d064747bf976f7011f
Nick Mitchell <nickm@us.ibm.com>
2024-02-17 13:14:50 -0500

    fix: dashboard/hack/update-controlplane should also restart defaults

--- platform/dashboard/hack/update-controlplane.sh
+++ platform/dashboard/hack/update-controlplane.sh
@@ -10,8 +10,12 @@ TOP="$SCRIPTDIR"/../../..
 . "$TOP"/hack/settings.sh
 
 $KUBECTL delete -f resources/jaas-examples.yml --ignore-not-found 1>&2
+$KUBECTL delete -f resources/jaas-defaults.yml --ignore-not-found 1>&2
 $KUBECTL delete -f resources/jaas-lite.yml --ignore-not-found 1>&2
-../../hack/build.sh # rebuild the controller images
-./hack/generate-installers.sh # the dashboard includes a precompiled version of the jaas charts
+
+# rebuild the controller images & the dashboard includes a precompiled version of the jaas charts
+../../hack/build.sh & ./hack/generate-installers.sh
+
 $KUBECTL apply -f resources/jaas-lite.yml 1>&2
+$KUBECTL apply -f resources/jaas-defaults.yml 1>&2
 $KUBECTL apply -f resources/jaas-examples.yml 1>&2
