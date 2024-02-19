7b48332d4bf78b4f6b0e2af8b1977dd87b63b149
Nick Mitchell <nickm@us.ibm.com>
2024-02-19 13:10:38 -0500

    fix: platform/dashboard/update-controlplane still uses prior jaas-examples.yml
    It should have been updated by #1107 to be jaas-default-user.yml
    
    Also:
    - no grace period for core delete
    - parallelize the operations a bit
    - platform/dashboard/update-controlplane is missing wait for hack/build

--- platform/dashboard/hack/update-controlplane.sh
+++ platform/dashboard/hack/update-controlplane.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-set -o pipefail
 set -x
 
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
@@ -9,13 +7,20 @@ TOP="$SCRIPTDIR"/../../..
 
 . "$TOP"/hack/settings.sh
 
-$KUBECTL delete -f resources/jaas-examples.yml --ignore-not-found || true
-$KUBECTL delete -f resources/jaas-defaults.yml --ignore-not-found || true
-$KUBECTL delete -f resources/jaas-lite.yml --ignore-not-found || true
+set +e
+$KUBECTL delete -f resources/jaas-default-user.yml --ignore-not-found & \
+    $KUBECTL delete -f resources/jaas-defaults.yml --ignore-not-found
+wait
+$KUBECTL delete -f resources/jaas-lite.yml --ignore-not-found --grace-period=1
+
+set -e
+set -o pipefail
 
 # rebuild the controller images & the dashboard includes a precompiled version of the jaas charts
 ../../hack/build.sh & ./hack/generate-installers.sh
+wait
 
-$KUBECTL apply -f resources/jaas-lite.yml 1>&2
-$KUBECTL apply -f resources/jaas-defaults.yml 1>&2
-$KUBECTL apply -f resources/jaas-examples.yml 1>&2
+$KUBECTL apply -f resources/jaas-lite.yml
+$KUBECTL apply -f resources/jaas-defaults.yml & \
+    $KUBECTL apply -f resources/jaas-default-user.yml
+wait
