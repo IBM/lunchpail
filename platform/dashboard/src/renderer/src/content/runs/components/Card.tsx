1944e2d34bf90a720bce91e3626b3951655309b1
Nick Mitchell <nickm@us.ibm.com>
2024-02-26 08:58:34 -0500

    feat: remove one of the two links from Run Card to WorkerPools
    we had one "Associated Worker Pools" as a description group item, and then separately we had a second in the ProgressStepper. this change removes the former.

--- platform/dashboard/src/renderer/src/content/runs/components/Card.tsx
+++ platform/dashboard/src/renderer/src/content/runs/components/Card.tsx
@@ -1,17 +1,13 @@
 import { useMemo } from "react"
 import { Label, LabelGroup } from "@patternfly/react-core"
 
-import None from "@jaas/components/None"
 import CardInGallery from "@jaas/components/CardInGallery"
-import { linkToAllDetails } from "@jaas/renderer/navigate/details"
 import { descriptionGroup } from "@jaas/components/DescriptionGroup"
 
 import api from "@jaas/resources/applications/components/api"
 import { taskqueue as associatedTaskQueue } from "@jaas/resources/runs/components/taskqueueProps"
 import ProgressStepper from "@jaas/resources/runs/components/ProgressStepper"
 import { done, unassigned } from "@jaas/resources/taskqueues/components/unassigned"
-
-import { name as workerpoolsName } from "@jaas/resources/workerpools/name"
 
 import type Props from "./Props"
 
@@ -36,34 +32,8 @@ function tags(tags: Props["application"]["spec"]["tags"]) {
       }
 }
 
-/** @return set of WorkerPools assigned to this `Run` */
-function associatedWorkerPools(
-  props: Pick<Props, "run" | "workerpools" | "taskqueues">,
-  queue = associatedTaskQueue(props),
-) {
-  return !queue
-    ? []
-    : props.workerpools.filter(
-        ({ metadata, spec }) =>
-          spec.run.name === props.run.metadata.name && metadata.context === queue.metadata.context,
-      )
-}
-
-/** @return description group for `WorkerPools` associated with this `Run` */
-function workerpoolsGroup(props: Pick<Props, "workerpools">) {
-  return props.workerpools.length === 0
-    ? undefined
-    : descriptionGroup(
-        `Active ${workerpoolsName}`,
-        props.workerpools.length === 0 ? None() : linkToAllDetails("workerpools", props.workerpools),
-        props.workerpools.length,
-        "The Worker Pools that have been assigned to process tasks from this queue.",
-      )
-}
-
 export default function RunCard(props: Props) {
   const taskqueue = associatedTaskQueue(props)
-  const workerpools = associatedWorkerPools(props, taskqueue)
 
   // Card actions
   const actions = useMemo(() => tags(props.application.spec.tags), [props.application.spec.tags?.join(";")])
@@ -75,9 +45,8 @@ export default function RunCard(props: Props) {
       props.application.spec.description && descriptionGroup("Description", props.application.spec.description),
       ...(!taskqueue ? [] : [unassigned({ taskqueue, run: props.run })]),
       ...(!taskqueue ? [] : [done({ taskqueue, run: props.run })]),
-      ...(!taskqueue ? [] : [workerpoolsGroup({ workerpools })]),
     ],
-    [props.run, props.application, JSON.stringify(taskqueue), JSON.stringify(workerpools)],
+    [props.run, props.application, JSON.stringify(taskqueue)],
   )
 
   return (
