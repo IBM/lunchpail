840dd18f76a52ab66e194a86c692fb971fb40ef3
Nick Mitchell <nickm@us.ibm.com>
2024-03-10 19:37:28 -0400

    refactor: code cleanup (comments, etc.) to workstealer code

--- platform/components/jaas/workstealer/main.go
+++ platform/components/jaas/workstealer/main.go
@@ -37,209 +37,218 @@ func main() {
 
 	fmt.Fprintf(os.Stderr, "[workstealer] Starting with inbox=%s outbox=%s queues=%s\n", inbox, outbox, queues)
 
-	// Keep track of how many tasks we have moved to the final
-	// outbox. TODO: don't start from 0, we need to scan the
-	// outbox to look for bits from prior instances of the
-	// WorkStealer (e.g. if the pod fails)
-	var nDone uint = 0
-
 	err := os.MkdirAll(outbox, 0700)
 	if err != nil {
 		log.Fatalf("[workstealer] Failed to create outbox directory: %v\n", err)
 		return
 	}
 
-	{
-		// Check for existence of unassigned inbox
-		if f, err := os.Stat(inbox); err == nil && f.IsDir() {
-			fmt.Fprintf(os.Stderr, "[workstealer] Scanning inbox: %s\n", inbox)
+	// Check for existence of unassigned inbox
+	if f, err := os.Stat(inbox); err == nil && f.IsDir() {
+		fmt.Fprintf(os.Stderr, "[workstealer] Scanning inbox: %s\n", inbox)
 
-			// We will enumerate the unassigned inbox to find the current unassigned work items
-			f, err := os.Open(inbox)
-			if err != nil {
-				log.Fatalf("[workstealer] Failed to read inbox directory: %v\n", err)
-				return
+		// We will enumerate the unassigned inbox to find the current unassigned work items
+		f, err := os.Open(inbox)
+		if err != nil {
+			log.Fatalf("[workstealer] Failed to read inbox directory: %v\n", err)
+			return
+		}
+
+		// Here is the readdir/enumeration of the unassigned directory
+		files, err := f.Readdir(0)
+		if err != nil {
+			log.Fatalf("[workstealer] Failed to read contents of inbox directory: %v\n", err)
+		}
+
+		// We will tally up the total number of tasks (nFiles) and the number already assigned to
+		// a worker (nAssigned)
+		var nFiles uint = 0
+		var nAssigned uint = 0
+
+		// Keep track of how many tasks we have moved to the final
+		// outbox. TODO: don't start from 0, we need to scan the
+		// outbox to look for bits from prior instances of the
+		// WorkStealer (e.g. if the pod fails)
+		var nDone uint = 0
+
+		// Here is the loop over files in the unassigned inbox directory
+		for _, file := range files {
+			fileName := file.Name()
+			if strings.HasSuffix(fileName, ".lock") || strings.HasSuffix(fileName, ".done") {
+				// Skip over the marker files
+				continue
 			}
 
-			// Here is the readdir/enumeration of the unassigned directory
-			files, err := f.Readdir(0)
-			if err != nil {
-				log.Fatalf("[workstealer] Failed to read contents of inbox directory: %v\n", err)
+			// We want nUnassigned... we will compute this
+			// by tallying nFiles (total number of files
+			// in global inbox) and nAssigned (the subset
+			// already assigned to a worker) and take the
+			// difference at the end.
+			nFiles++
+
+			filePath := filepath.Join(inbox, fileName)
+			doneMarker := filePath+".done"
+			lockMarker := filePath+".lock"
+
+			// Check if the file is already done (worker finished processing)
+			if _, err := os.Stat(doneMarker); err == nil {
+				// Then this task is already flagged as done
+				nDone++
+				nAssigned++
+				fmt.Fprintf(os.Stderr, "[workstealer] skipping already-done file=%s nAassigned=%d\n", fileName, nAssigned)
+				continue
 			}
 
-			// We will tally up the total number of tasks (nFiles) and the number already assigned to
-			// a worker (nAssigned)
-			var nFiles uint = 0
-			var nAssigned uint = 0
-
-			// Here is the loop over files in the unassigned inbox directory
-			for _, file := range files {
-				fileName := file.Name()
-				if strings.HasSuffix(fileName, ".lock") || strings.HasSuffix(fileName, ".done") {
-					// skip over the lock files
-					continue
-				}
-
-				// keep track of how many we have yet to assign
-				nFiles++
-				filePath := filepath.Join(inbox, fileName)
-				doneMarker := filePath+".done"
-				lockMarker := filePath+".lock"
-
-				if _, err := os.Stat(doneMarker); err == nil {
-					// the work is already flagged as done
-					nAssigned++
-					fmt.Fprintf(os.Stderr, "[workstealer] skipping already-done file=%s nAassigned=%d\n", fileName, nAssigned)
-					continue
-				}
-
-				if _, err := os.Stat(lockMarker); err == nil {
-					// Then this task is locked, i.e. already assigned to a worker.
-					lockFile, err := os.ReadFile(lockMarker)
-					if err != nil {
-						log.Fatalf("[workstealer] Failed to read lock file: %v\n", err)
-					}
-
-					// The lock file contents will the id of the owning worker
-					worker := strings.TrimSpace(string(lockFile))
-
-					// Check the worker's outbox for the task
-					fileInWorkerOutbox := filepath.Join(worker, "outbox", fileName)
-
-					fmt.Fprintf(os.Stderr, "[workstealer] checking worker outbox %s\n", fileInWorkerOutbox)
-					if _, err := os.Stat(fileInWorkerOutbox); err == nil {
-						// Then yes, this task is done. Flag it as such: increment nAssigned,
-						// touch the .done file and remove the .lock file
-						nAssigned++
-						fmt.Fprintf(os.Stderr, "[workstealer] skipping already-done (2) file=%s nAssigned=%d\n", fileName, nAssigned)
-
-						// ...touch the done file
-						if err := os.WriteFile(doneMarker, []byte{}, 0644); err != nil {
-							log.Fatalf("[workstealer] Failed to touch done file: %v\n", err)
-						} else {
-							reportChangedFile(doneMarker)
-						}
-
-						// ...remove the lock file
-						if err := os.Remove(lockMarker); err != nil {
-							log.Fatalf("[workstealer] Failed to remove lock file: %v\n", err)
-						} else {
-							reportChangedFile(lockMarker)
-						}
-
-						// move the output to the final/global (i.e. not per-worker) outbox
-						fullyDoneOutputFilePath := filepath.Join(outbox, fileName)
-						err := os.Rename(fileInWorkerOutbox, fullyDoneOutputFilePath)
-						nDone++
-						if err != nil {
-							log.Fatalf("[workstealer] Failed to copy output to final outbox: %v\n", err)
-						} else {
-							reportChangedFile(fullyDoneOutputFilePath)
-						}
-
-						// Nothing more to do for this task file
-						continue
-					}
-				}
-
-				// If we get here, then we have an unassigned task. Pick a worker randomly
-				// and send the task to that worker's queue.
-				workerDirs, err := filepath.Glob(filepath.Join(queues, "*"))
+			// Check if the file is locked (assigned to a worker)
+			if _, err := os.Stat(lockMarker); err == nil {
+				// Then this task is locked, i.e. already assigned to a worker.
+				lockFile, err := os.ReadFile(lockMarker)
 				if err != nil {
-					log.Fatalf("[workstealer] Failed to get worker directories: %v\n", err)
+					log.Fatalf("[workstealer] Failed to read lock file: %v\n", err)
 				}
 
-				if len(workerDirs) == 0 {
-					fmt.Fprintln(os.Stderr, "[workstealer] Warning: no queues ready")
+				// The lock file contents will the id of the owning worker
+				worker := strings.TrimSpace(string(lockFile))
+
+				// Check the worker's outbox for the task
+				fileInWorkerOutbox := filepath.Join(worker, "outbox", fileName)
+
+				// The file is locked by a worker, but
+				// perhaps the worker has already
+				// completed this task
+				fmt.Fprintf(os.Stderr, "[workstealer] checking worker outbox %s\n", fileInWorkerOutbox)
+				if _, err := os.Stat(fileInWorkerOutbox); err == nil {
+					// Then yes, this task is done. Flag it as such: increment nAssigned,
+					// touch the .done file and remove the .lock file
+					nAssigned++
+					fmt.Fprintf(os.Stderr, "[workstealer] skipping already-done (2) file=%s nAssigned=%d\n", fileName, nAssigned)
+
+					// ...touch the done file
+					if err := os.WriteFile(doneMarker, []byte{}, 0644); err != nil {
+						log.Fatalf("[workstealer] Failed to touch done file: %v\n", err)
+					} else {
+						reportChangedFile(doneMarker)
+					}
+
+					// ...remove the lock file
+					if err := os.Remove(lockMarker); err != nil {
+						log.Fatalf("[workstealer] Failed to remove lock file: %v\n", err)
+					} else {
+						reportChangedFile(lockMarker)
+					}
+
+					// move the output to the final/global (i.e. not per-worker) outbox
+					fullyDoneOutputFilePath := filepath.Join(outbox, fileName)
+					err := os.Rename(fileInWorkerOutbox, fullyDoneOutputFilePath)
+					nDone++
+					if err != nil {
+						log.Fatalf("[workstealer] Failed to copy output to final outbox: %v\n", err)
+					} else {
+						reportChangedFile(fullyDoneOutputFilePath)
+					}
+
+					// Nothing more to do for this task file
 					continue
 				}
+			}
 
-				// Pick a random worker
-				workerDir := workerDirs[rand.Intn(len(workerDirs))]
-				queue := filepath.Join(workerDir, "inbox")
+			// If we get here, then we have an unassigned task. Pick a worker randomly
+			// and send the task to that worker's queue.
+			workerDirs, err := filepath.Glob(filepath.Join(queues, "*"))
+			if err != nil {
+				log.Fatalf("[workstealer] Failed to get worker directories: %v\n", err)
+			}
 
-				// Check if that worker is no longer alive
-				if _, err := os.Stat(filepath.Join(queue, ".alive")); os.IsNotExist(err) {
-					/* TODO: maybe we need to loop more tightly
+			if len(workerDirs) == 0 {
+				fmt.Fprintln(os.Stderr, "[workstealer] Warning: no queues ready")
+				continue
+			}
+
+			// Pick a random worker
+			workerDir := workerDirs[rand.Intn(len(workerDirs))]
+			queue := filepath.Join(workerDir, "inbox")
+
+			// Check if that worker is no longer alive
+			if _, err := os.Stat(filepath.Join(queue, ".alive")); os.IsNotExist(err) {
+				/* TODO: maybe we need to loop more tightly
 					   here over possibly available workers?
 					   otherwise, we may delay 5 seconds in
 					   assigning a task, even when there are other
 					   workers that *are* active? */
-					fmt.Fprintf(os.Stderr, "[workstealer] skipping inactive queue=%s\n", queue)
+				fmt.Fprintf(os.Stderr, "[workstealer] skipping inactive queue=%s\n", queue)
 
-					// If the worker has any assigned tasks, unlock those files owned by that worker
-					lockFiles, err := filepath.Glob(filepath.Join(inbox, "*.lock"))
+				// If the worker has any assigned tasks, unlock those files owned by that worker
+				lockFiles, err := filepath.Glob(filepath.Join(inbox, "*.lock"))
+				if err != nil {
+					log.Fatalf("[workstealer] Failed to get lock files: %v\n", err)
+				}
+
+				// Iterate over files locked by this worker
+				for _, lockFile := range lockFiles {
+					lockContent, err := os.ReadFile(lockFile)
 					if err != nil {
-						log.Fatalf("[workstealer] Failed to get lock files: %v\n", err)
+						log.Fatalf("[workstealer] Failed to read lock file: %v\n", err)
 					}
-
-					// Iterate over files locked by this worker
-					for _, lockFile := range lockFiles {
-						lockContent, err := os.ReadFile(lockFile)
-						if err != nil {
-							log.Fatalf("[workstealer] Failed to read lock file: %v\n", err)
+					if strings.TrimSpace(string(lockContent)) == workerDir {
+						doneFile := filepath.Join(workerDir, "outbox", strings.TrimSuffix(filepath.Base(lockFile), ".lock"))
+						fmt.Fprintf(os.Stderr, "[workstealer] Checking if task is done: %s\n", doneFile)
+						_, err := os.Stat(doneFile)
+						if err == nil {
+							fmt.Fprintf(os.Stderr, "[workstealer] Removing finished task owned by dead worker=%s filelock=%s\n", workerDir, lockFile)
+							filePath = strings.TrimSuffix(filepath.Base(lockFile), ".lock")
+							if err := os.WriteFile(doneMarker, []byte{}, 0644); err != nil {
+								log.Fatalf("[workstealer] Failed to touch done file: %v\n", err)
+							} else {
+								reportChangedFile(doneMarker)
+							}
+						} else {
+							fmt.Fprintf(os.Stderr, "[workstealer] Unlocking task owned by dead worker=%s filelock=%s", workerDir, lockFile)
 						}
-						if strings.TrimSpace(string(lockContent)) == workerDir {
-							doneFile := filepath.Join(workerDir, "outbox", strings.TrimSuffix(filepath.Base(lockFile), ".lock"))
-							fmt.Fprintf(os.Stderr, "[workstealer] Checking if task is done: %s\n", doneFile)
-							_, err := os.Stat(doneFile)
-							if err == nil {
-								fmt.Fprintf(os.Stderr, "[workstealer] Removing finished task owned by dead worker=%s filelock=%s\n", workerDir, lockFile)
-								filePath = strings.TrimSuffix(filepath.Base(lockFile), ".lock")
-								if err := os.WriteFile(doneMarker, []byte{}, 0644); err != nil {
-									log.Fatalf("[workstealer] Failed to touch done file: %v\n", err)
-								} else {
-									reportChangedFile(doneMarker)
-								}
-							} else {
-								fmt.Fprintf(os.Stderr, "[workstealer] Unlocking task owned by dead worker=%s filelock=%s", workerDir, lockFile)
-							}
-							if err := os.Remove(lockFile); err != nil {
-								log.Fatalf("[workstealer] Failed to remove lock file: %v\n", err)
-							} else {
-								reportChangedFile(lockFile)
-							}
+						if err := os.Remove(lockFile); err != nil {
+							log.Fatalf("[workstealer] Failed to remove lock file: %v\n", err)
+						} else {
+							reportChangedFile(lockFile)
 						}
 					}
-
-					continue
 				}
 
-				if _, err := os.Stat(lockMarker); err == nil {
-					nAssigned++
-					fmt.Fprintf(os.Stderr, "[workstealer] skipping already-locked file=%s nAssigned=%d\n", fileName, nAssigned)
-				} else if fi, err := os.Stat(queue); err == nil && fi.IsDir() && fileName != "" {
-					// here is where we assign a task to a worker
-					nAssigned++
-					fmt.Fprintf(os.Stderr, "[workstealer] Moving task=%s to queue=%s nAssigned=%d\n", fileName, queue, nAssigned)
-					filePathToWorkerInbox := filepath.Join(queue, fileName)
-
-					os.WriteFile(lockMarker, []byte(workerDir), 0644)
-					data, _ := os.ReadFile(filepath.Join(inbox, fileName))
-					os.WriteFile(filePathToWorkerInbox, data, 0644)
-
-					reportChangedFile(lockMarker)
-					reportChangedFile(filePathToWorkerInbox)
-				} else {
-					fmt.Fprintf(os.Stderr, "[workstealer] Warning: strange! Unable to assign task to a worker: %s\n", fileName)
-					if _, err := os.Stat(queue); os.IsNotExist(err) {
-						fmt.Fprintf(os.Stderr, "[workstealer] Warning: Not a directory=%s\n", queue)
-					}
-					if fileName == "" {
-						fmt.Fprintln(os.Stderr, "[workstealer] Warning: Empty")
-					}
-					if _, err := os.Stat(filepath.Join(inbox, fileName)); os.IsNotExist(err) {
-						fmt.Fprintf(os.Stderr, "[workstealer] Warning: Not a file task=%s\n", filepath.Join(inbox, fileName))
-					}
-					if _, err := os.Stat(lockMarker); err == nil {
-						lockFile, _ := os.ReadFile(lockMarker)
-						fmt.Fprintf(os.Stderr, "[workstealer] Warning: Already owned %s\n", string(lockFile))
-					}
-				}
+				continue
 			}
 
-			reportUnassigned(nFiles - nAssigned)
-			reportDone(nDone)
+			if _, err := os.Stat(lockMarker); err == nil {
+				nAssigned++
+				fmt.Fprintf(os.Stderr, "[workstealer] skipping already-locked file=%s nAssigned=%d\n", fileName, nAssigned)
+			} else if fi, err := os.Stat(queue); err == nil && fi.IsDir() && fileName != "" {
+				// here is where we assign a task to a worker
+				nAssigned++
+				fmt.Fprintf(os.Stderr, "[workstealer] Moving task=%s to queue=%s nAssigned=%d\n", fileName, queue, nAssigned)
+				filePathToWorkerInbox := filepath.Join(queue, fileName)
+
+				os.WriteFile(lockMarker, []byte(workerDir), 0644)
+				data, _ := os.ReadFile(filepath.Join(inbox, fileName))
+				os.WriteFile(filePathToWorkerInbox, data, 0644)
+
+				reportChangedFile(lockMarker)
+				reportChangedFile(filePathToWorkerInbox)
+			} else {
+				fmt.Fprintf(os.Stderr, "[workstealer] Warning: strange! Unable to assign task to a worker: %s\n", fileName)
+				if _, err := os.Stat(queue); os.IsNotExist(err) {
+					fmt.Fprintf(os.Stderr, "[workstealer] Warning: Not a directory=%s\n", queue)
+				}
+				if fileName == "" {
+					fmt.Fprintln(os.Stderr, "[workstealer] Warning: Empty")
+				}
+				if _, err := os.Stat(filepath.Join(inbox, fileName)); os.IsNotExist(err) {
+					fmt.Fprintf(os.Stderr, "[workstealer] Warning: Not a file task=%s\n", filepath.Join(inbox, fileName))
+				}
+				if _, err := os.Stat(lockMarker); err == nil {
+					lockFile, _ := os.ReadFile(lockMarker)
+					fmt.Fprintf(os.Stderr, "[workstealer] Warning: Already owned %s\n", string(lockFile))
+				}
+			}
 		}
+
+		reportUnassigned(nFiles - nAssigned)
+		reportDone(nDone)
 	}
 }
--- platform/components/jaas/workstealer/workstealer.sh
+++ platform/components/jaas/workstealer/workstealer.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+SCRIPTDIR=$(cd $(dirname "$0") && pwd)
+
 LOCAL_QUEUE_ROOT=$(mktemp -d)
 QUEUE_PATH=${!REMOTE_PATH_VAR}/$RUN_NAME
 export QUEUE=$LOCAL_QUEUE_ROOT/$QUEUE_PATH
@@ -10,6 +12,7 @@ printenv
 config=/tmp/rclone.conf
 remote=s3:/$QUEUE_PATH
 
+# the rclone.conf file
 cat <<EOF > $config
 [s3]
 type = s3
@@ -21,11 +24,16 @@ secret_access_key = ${!AWS_SECRET_ACCESS_KEY_VAR}
 acl = public-read
 EOF
 
-# otherwise inotifywait won't have an inode to watch...
+# We need to do this up front, otherwise inotifywait won't have an
+# inode to watch
 mkdir -p $QUEUE
 
-SCRIPTDIR=$(cd $(dirname "$0") && pwd)
-(inotifywait -r -m -e create -e moved_to $QUEUE |
+# -r: look recursively for changes
+# -m: keep on watching ("monitor" mode)
+# -e create: tell us when a file is created
+# -e moved_to: tell us when a file is renamed
+# -e delete: tell us when a file is deleted
+(inotifywait -r -m -e create -e moved_to -e delete $QUEUE |
      while read directory action file
      do
          if [[ "$action" = "CREATE,ISDIR" ]]
@@ -38,18 +46,25 @@ SCRIPTDIR=$(cd $(dirname "$0") && pwd)
          then continue
          fi
 
+         # Launch the go code, which will emit a newline-separated
+         # stream of files it has changed; here we react to those
+         # changes by uploading back to the remote using `rclone
+         # copyto`
          echo "Launching workstealer processor due to change directory=$directory action=$action file=$file"
          "$SCRIPTDIR"/workstealer | while read file
          do
              remotefile=s3:$(echo $file | sed -E "s#^$LOCAL_QUEUE_ROOT/##")
              echo "Uploading changed file: $file -> $remotefile"
-             rclone --config /tmp/rclone.conf copyto $file $remotefile
+             rclone --config $config copyto $file $remotefile
          done
      done
 ) &
 
+# Poll for changes to the remote, using `rclone sync` to copy them
+# locally. Then, the above inotifywait will be ... notified and then
+# react to those changes.
 while true
 do
-    rclone --config /tmp/rclone.conf sync --exclude '*.partial' $remote $QUEUE
+    rclone --config $config sync --exclude '*.partial' $remote $QUEUE
     sleep ${QUEUE_POLL_INTERVAL_SECONDS:-3}
 done
