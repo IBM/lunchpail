cb07d44a22bf58aa63ef7b4b414544ef83e26977
Nick Mitchell <nickm@us.ibm.com>
2024-03-12 18:50:17 -0400

    fix: workstealer's use of inotifywait is pretty buggy
    We may need to debounce our use of it. In this change, instead we avoid it and just invoke the go code if the sync'd directory has changed.

--- platform/components/jaas/workstealer/workstealer.sh
+++ platform/components/jaas/workstealer/workstealer.sh
@@ -26,61 +26,86 @@ EOF
 
 # We need to do this up front, otherwise inotifywait won't have an
 # inode to watch
-mkdir -p $QUEUE
-
+# mkdir -p $QUEUE
+#
 # -r: look recursively for changes
 # -m: keep on watching ("monitor" mode)
 # -e create: tell us when a file is created
 # -e moved_to: tell us when a file is renamed
 # -e delete: tell us when a file is deleted
-(inotifywait -r -m -e create -e moved_to -e delete $QUEUE |
-     while read directory action file
-     do
-         if [[ "$action" = "CREATE,ISDIR" ]] || [[ "$action" = "DELETE,ISDIR" ]]
-         then continue
-         elif [[ "$file" =~ ".lock" ]]
-         then continue
-         elif [[ "$file" =~ ".done" ]]
-         then continue
-         elif [[ "$file" =~ ".partial" ]]
-         then continue
-         fi
+# (inotifywait -r -m -e create -e moved_to -e delete $QUEUE |
+#      while read directory action file
+#      do
+#          if [[ "$action" = "CREATE,ISDIR" ]] || [[ "$action" = "DELETE,ISDIR" ]]
+#          then continue
+#          elif [[ "$file" =~ ".lock" ]]
+#          then continue
+#          elif [[ "$file" =~ ".done" ]]
+#          then continue
+#          elif [[ "$file" =~ ".partial" ]]
+#          then continue
+#          fi
 
-         # Launch the go code, which will emit a newline-separated
-         # stream of files it has changed; here we react to those
-         # changes by uploading back to the remote using `rclone
-         # copyto`
-         echo "Launching workstealer processor due to change directory=$directory action=$action file=$file"
-         find $QUEUE
-         echo "-----------------------------------------"
+#          # Launch the go code, which will emit a newline-separated
+#          # stream of files it has changed; here we react to those
+#          # changes by uploading back to the remote using `rclone
+#          # copyto`
+#          echo "Launching workstealer processor due to change directory=$directory action=$action file=$file"
+#          find $QUEUE
+#          echo "-----------------------------------------"
          
-         "$SCRIPTDIR"/workstealer | while read file
-         do
-             remotefile=s3:$(echo $file | sed -E "s#^$LOCAL_QUEUE_ROOT/##")
-             echo "Uploading changed file: $file -> $remotefile"
-             rclone --config $config copyto --retries 20 --retries-sleep=1s $file $remotefile
-         done
-     done
-) &
+#          "$SCRIPTDIR"/workstealer | while read file
+#          do
+#              remotefile=s3:$(echo $file | sed -E "s#^$LOCAL_QUEUE_ROOT/##")
+#              echo "Uploading changed file: $file -> $remotefile"
+#              rclone --config $config copyto --retries 20 --retries-sleep=1s $file $remotefile
+#          done
+#      done
+# ) &
 
-# hmm, we may need to wait a bit for the inotifywait to register
-sleep 5
+# # hmm, we may need to wait a bit for the inotifywait to register
+# sleep 5
+
+B=$(mktemp)
+A=$(mktemp)
 
 # Poll for changes to the remote, using `rclone sync` to copy them
 # locally. Then, the above inotifywait will be ... notified and then
 # react to those changes.
 while true
 do
-    echo "About to clone"
-    rclone --config $config tree $remote
-    echo "+++++++++++++++++++++++++++++++++++++++++++++++"
-
-    rclone --config $config sync --create-empty-src-dirs --retries 20 --retries-sleep=1s --update --exclude '*.partial' $remote $QUEUE
+    find $QUEUE | grep -Ev '.lock|.done' > $B
+    rclone --config $config sync --create-empty-src-dirs --retries 20 --retries-sleep=1s --exclude '*/processing/*' --update --exclude '*.partial' $remote $QUEUE
 
     if [[ $? != 0 ]]
     then
+        # then the rclone failed
         echo "Error with rclone sync. Here is an rclone tree of remote=$remote"
         rclone --config $config tree $remote
+    else
+        find $QUEUE | grep -Ev '.lock|.done' > $A
+
+        beforesum=$(cat $B | grep -Ev '.lock|.done|.partial' | sha256sum)
+        aftersum=$(cat $A | grep -Ev '.lock|.done|.partial' | sha256sum)
+        if [[ $beforesum != $aftersum ]]
+        then
+            # Then we sync'd in some updates. Launch the go code, which
+            # will emit a newline-separated stream of files it has
+            # changed; here we react to those changes by uploading back to
+            # the remote using `rclone copyto`
+
+            echo "Launching workstealer processor due to change $beforesum != $aftersum"
+
+            # Identify changes
+            # diff $B $A | tail -n +5 | grep -Ev '^ '
+
+            "$SCRIPTDIR"/workstealer | while read file
+            do
+                remotefile=s3:$(echo $file | sed -E "s#^$LOCAL_QUEUE_ROOT/##")
+                echo "Uploading changed file: $file -> $remotefile"
+                rclone --config $config copyto --retries 20 --retries-sleep=1s $file $remotefile
+            done
+        fi
     fi
 
     sleep ${QUEUE_POLL_INTERVAL_SECONDS:-3}
