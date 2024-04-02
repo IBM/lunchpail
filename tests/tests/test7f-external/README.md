9c6f7aae2a996d982618b29a2251969c016698d0
Nick Mitchell <nickm@us.ibm.com>
2024-04-02 09:53:11 -0400

    test: add test7f-external variant, pointing to lunchpail-demo repo

---
+++ tests/tests/test7f-external/README.md
@@ -0,0 +1,3 @@
+# test7f-external
+
+As with test7f, but pointing to an external repo for the pail.
---
+++ tests/tests/test7f-external/settings.sh
@@ -0,0 +1,13 @@
+api=workqueue
+#app=git@github.ibm.com:cloud-computer/lunchpail-demo.git
+#branch=v0.0.2
+app=git@github.ibm.com:nickm/lunchpail-demo.git
+branch=subdirs
+deployname=lunchpail-demo
+
+# /queue/0,1 <-- 2 workers
+# task.1,task.3,task.5 <-- 3 tasks per iter
+
+# "tasky" and 3333 come from a values override in the workdispatcher.yaml
+
+expected=('Processing /queue/[[:digit:]]+/processing/tasky3333.1.txt' 'Processing /queue/[[:digit:]]+/processing/tasky3333.3.txt' 'Processing /queue/[[:digit:]]+/processing/tasky3333.5.txt' 'Processing /queue/[[:digit:]]+/processing/tasky3333.2.txt' 'Processing /queue/[[:digit:]]+/processing/tasky3333.4.txt' 'Processing /queue/[[:digit:]]+/processing/tasky3333.6.txt')
