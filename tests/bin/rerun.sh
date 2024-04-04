8497f5378c2bed5126fa9695c14d647610ed5c31
Nick Mitchell <nickm@us.ibm.com>
2024-04-04 14:11:49 -0400

    refactor: update rerun.sh to use new update.sh script

--- tests/bin/rerun.sh
+++ tests/bin/rerun.sh
@@ -6,17 +6,9 @@ set -o pipefail
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 TOP="$SCRIPTDIR"/../..
 
-"$TOP"/hack/build.sh -l > /dev/null &
-
 if [[ -f "$TOP"/builds/test/$(basename $1)/down ]]
 then "$TOP"/builds/test/$(basename $1)/down
 fi
 
-"$TOP"/builds/lite/down
-"$TOP"/hack/shrinkcore.sh "$TOP"/builds
-
-wait
-
-"$TOP"/builds/lite/up
-
+"$TOP"/hack/update.sh
 "$SCRIPTDIR"/run.sh $1
