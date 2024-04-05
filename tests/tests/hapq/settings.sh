b1a09c257e332366b669d82165f87e173bc7484d
Nick Mitchell <nickm@us.ibm.com>
2024-04-05 13:16:42 -0400

    refactor: externalize hapq example

--- tests/tests/hapq/settings.sh
+++ tests/tests/hapq/settings.sh
@@ -1,5 +1,6 @@
 # hapq test
-app="$TOP"/watsonx_ai/preprocessing/hapq
+app=git@github.ibm.com:cloud-computer/lunchpail-hapq.git
+branch=v0.0.1
 expected=('HAPq processing input_file=')
 
 if which lspci && lspci | grep -iq nvidia; then
