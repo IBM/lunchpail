3d248a5d1c3f3949890393c3eee57678e8d2a3b5
Nick Mitchell <nickm@us.ibm.com>
2024-05-12 13:10:02 -0400

    feat: use ansi box dividers in status output

--- pkg/shrinkwrap/status/ui.go
+++ pkg/shrinkwrap/status/ui.go
@@ -57,57 +57,104 @@ func WaitForRun(runname, namespace string, wait bool, verbose bool) (string, str
 	return appname, runname, nil
 }
 
-func view(status Status) []string {
+func max(nums ...int) int {
+	max := 0
+	for _, n := range nums {
+		if n > max {
+			max = n
+		}
+	}
+	return max
+}
+
+func rspacex(str string, actualSpace, availableSpace int) string {
+	// - 2 as availableSpace includes one space character on either side
+	return str + strings.Repeat(" ", max(0, availableSpace - actualSpace - 2))
+}
+
+func rspace(str string, availableSpace int) string {
+	return rspacex(str, len(str), availableSpace)
+}
+
+func rspacef(num, denom int, str string, availableSpace int) string {
+	fullstr := fmt.Sprintf("%d/%d %s", num, denom, str)
+	return rspace(fullstr, availableSpace)
+}
+
+func rspacef1(num int, status WorkerStatus, availableSpace int) string {
+	frac := fmt.Sprintf("%d/1", num)
+	fullstr := fmt.Sprintf("%s %s", frac, cell(status))
+	return rspacex(fullstr, len(frac) + 2, availableSpace) // +1 for cell, +1 for space
+}
+
+func rspacefw(num int, denom int, workers []Worker, availableSpace int) string {
+	frac := fmt.Sprintf("%d/%d", num, denom)
+	fullstr := fmt.Sprintf("%s %s", frac, workerCells(workers))
+	return rspacex(fullstr, len(frac) + 2, availableSpace)
+}
+
+func view(status Status) {
 	dim := lipgloss.NewStyle().Faint(true)
 	bold := lipgloss.NewStyle().Bold(true)
 	cyan := lipgloss.NewStyle().Foreground(lipgloss.Color("6"))
 
 	runningWorkers, totalWorkers := status.workersSplit()
-	runningRuntime, totalRuntime := status.split(status.Runtime)
-	runningInternalS3, totalInternalS3 := status.split(status.InternalS3)
-	runningWorkStealer, totalWorkStealer := status.split(status.WorkStealer)
+	runningRuntime, _ := status.split(status.Runtime)
+	runningInternalS3, _ := status.split(status.InternalS3)
+	runningWorkStealer, _ := status.split(status.WorkStealer)
 
+	// 2 = 1 for left pad, 1 for right pad
+	// 4 = 1 for left pad, 1 for right pad, 1 for space between fraction and cells, 1 for fraction slash,
+	nrightbars := max(
+			8,
+			2 + len(status.AppName),
+			2 + len(status.RunName),
+			4 + totalWorkers + len(strconv.Itoa(runningWorkers)) + len(strconv.Itoa(totalWorkers)),
+		)
+	rightbars := strings.Repeat(
+		"─",
+		nrightbars,
+	)
+	leftbars := "──────────────"
+	topdiv := "┌" + leftbars + "┬" + rightbars + "┐"
+	middiv := "│" + leftbars + "┼" + rightbars + "│"
+	botdiv := "└" + leftbars + "┴" + rightbars + "┘"
+	
 	timestamp := status.LastEvent.Timestamp.Time
 	if timestamp.IsZero() {
 		timestamp = time.Now()
 	}
 
-	msgs := []string{
-		dim.Render(timestamp.Format(time.RFC850)),
-		fmt.Sprintf("%-20s ▏%s", bold.Render("App"), cyan.Render(status.AppName)),
-		fmt.Sprintf("%-20s ▏%s", bold.Render("Run"), cyan.Render(status.RunName)),
-		fmt.Sprintf("%-20s ▏%s", bold.Render("Pools"), cyan.Render(strconv.Itoa(status.numPools()))),
-
-		fmt.Sprintf("%-20s ▏%d/%d %s",
-			bold.Render("Workers"),
-			runningWorkers, totalWorkers,
-			workerCells(status.workers()),
-		),
-
-		fmt.Sprintf("%-20s ▏%d/%d %s",
-			bold.Render("Queue"),
-			runningInternalS3, totalInternalS3,
-			cell(status.InternalS3),
-		),
-
-		fmt.Sprintf("%-20s ▏%d/%d %s",
-			bold.Render("Runtime"),
-			runningRuntime, totalRuntime,
-			cell(status.Runtime),
-		),
-
-		fmt.Sprintf("%-20s ▏%d/%d %s",
-			bold.Render("WorkStealer"),
-			runningWorkStealer, totalWorkStealer,
-			cell(status.Runtime),
-		),
+	fmt.Printf(" %s\n", dim.Render(timestamp.Format(time.RFC850)))
+	fmt.Println(topdiv)
+	fmt.Printf("│ %-20s │ %s │\n", bold.Render("App"), cyan.Render(rspace(status.AppName, nrightbars)))
+	fmt.Printf("│ %-20s │ %s │\n", bold.Render("Run"), cyan.Render(rspace(status.RunName, nrightbars)))
+	fmt.Println(middiv)
+	fmt.Printf("│ %-20s │ %s │\n",
+		bold.Render("Queue"),
+		rspacef1(runningInternalS3, status.InternalS3, nrightbars),
+	)
+	fmt.Printf("│ %-20s │ %s │\n",
+		bold.Render("Runtime"),
+		rspacef1(runningRuntime, status.Runtime, nrightbars),
+	)
+	fmt.Printf("│ %-20s │ %s │\n",
+		bold.Render("WorkStealer"),
+		rspacef1(runningWorkStealer, status.Runtime, nrightbars),
+	)
+	fmt.Println(middiv)
+	fmt.Printf("│ %-20s │ %s │\n", bold.Render("Pools"), cyan.Render(rspace(strconv.Itoa(status.numPools()), nrightbars)))
+	for poolIdx, pool := range status.Pools {
+		fmt.Printf("│ %-20s │ %s │\n",
+			bold.Render("Pool " + strconv.Itoa(poolIdx + 1)), // TODO pool.Name
+			rspacefw(runningWorkers, totalWorkers, pool.Workers, nrightbars),
+		)
 	}
+	fmt.Println(botdiv)
 
 	if status.LastEvent.Message != "" {
-		msgs = append(msgs, dim.Render(status.LastEvent.Message))
+		fmt.Println(dim.Render(status.LastEvent.Message))
 	}
-
-	return msgs
 }
 
 func workerCells(workers []Worker) string {
@@ -139,8 +186,6 @@ func cell(status WorkerStatus) string {
 	}
 
 	cell := "■"
-	// cell := "◼"
-	// cell := "▇▇"
 	return style.Render(cell)
 }
 
@@ -166,16 +211,12 @@ func UI(runnameIn string, opts Options) error {
 
 	clearScreen(os.Stdout)
 
-	var val []string
 	for status := range c {
-		if !opts.Verbose && len(val) > 0 {
+		if !opts.Verbose {
 			clearScreen(os.Stdout)
 		}
 
-		val = view(status)
-		for _, line := range val {
-			fmt.Println(line)
-		}
+		view(status)
 	}
 
 	return nil
