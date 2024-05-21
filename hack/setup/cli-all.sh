266f97ad6f13b63c4a281af089b3f184f29b0499
Nick Mitchell <nickm@us.ibm.com>
2024-05-21 10:19:46 -0400

    fix: update cli-all.sh hack to build in parallel

--- hack/setup/cli-all.sh
+++ hack/setup/cli-all.sh
@@ -2,10 +2,23 @@
 
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 
+msg="Downloading CLI dependencies"
+echo "$msg" && go get ./...
+
+# We need two passes of `go generate`
+#   - pass 1 to generate base bits (version.txt, etc.)
+#   - pass 2 to include base bits in the lunchpail-source.tar.gz bit
+msg="Integrating templates"
+echo "$msg" && go generate ./... && go generate ./...
+
 for os in darwin linux
 do
     for arch in amd64 arm64
     do
-        GOOS=$os GOARCH=$arch "$SCRIPTDIR"/cli.sh lunchpail-$os-$arch
+        echo "Building CLI os=$os arch=$arch"
+        GOOS=$os GOARCH=$arch CGO_ENABLED=0 go build -ldflags="-s -w" -o "$DST" cmd/main.go &
     done
 done
+
+wait
+echo "Done"
