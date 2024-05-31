3e7f108f45197b9fc79fc741eb010c2d9f907c85
Nick Mitchell <nickm@us.ibm.com>
2024-05-31 11:52:01 -0400

    fix: typo in comment

--- pkg/lunchpail/images/build/components.go
+++ pkg/lunchpail/images/build/components.go
@@ -20,7 +20,7 @@ func BuildComponents(cli ContainerCli, opts BuildOptions) error {
 	errs.SetLimit(2) // podman doesn't like lots of concurrent pushes
 
 	if opts.Force && !opts.Production {
-		// HACK ALERT. Podman is stupid. It sometimes get
+		// HACK ALERT. Podman is stupid. It sometimes gets
 		// stuck on images with the wrong arch. This can
 		// happen if you have just built cross-platform
 		// manifests, and now want to build a single-platform
