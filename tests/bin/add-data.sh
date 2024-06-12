c7dca4830d248cac512afe043f2f0b251396e453
Nick Mitchell <nickm@us.ibm.com>
2024-06-12 14:26:49 -0400

    fix: tests that use add-data may fail if workstealer container is not ready

--- tests/bin/add-data.sh
+++ tests/bin/add-data.sh
@@ -11,15 +11,21 @@ set -o pipefail
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 
 # app.kubernetes.io/component label of pod that houses local s3
-S3C=workstealer
+selector=app.kubernetes.io/component=workstealer
 
 for bucket_path in $@; do
     if [[ -d $bucket_path ]]; then
         bucket=$(basename $bucket_path)
         echo "$(tput setaf 2)Populating s3 bucket $bucket from $bucket_path$(tput sgr0)"
 
-        pod=$(kubectl get pod -l app.kubernetes.io/component=$S3C -n $NAMESPACE --no-headers -o custom-columns=NAME:.metadata.name)
+        pod=$(kubectl get pod -l $selector -n $NAMESPACE --no-headers -o custom-columns=NAME:.metadata.name)
 
+        while true
+        do
+            kubectl wait pod $pod -n $NAMESPACE --for=condition=Ready --timeout=5s && break
+            sleep 1
+        done
+        
         set -x
         kubectl cp $bucket_path $pod:/tmp/$bucket -n $NAMESPACE
         kubectl exec $pod -n $NAMESPACE -- sh -c "rclone copy /tmp/$bucket s3:/$bucket && rm -rf /tmp/$bucket"
