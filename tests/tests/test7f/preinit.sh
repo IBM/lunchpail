6d4775ae32e5e78385f4922990a60ccf5a2311f9
Nick Mitchell <nickm@us.ibm.com>
2024-07-25 11:42:50 -0400

    fix: update internal minio service name to include 'lunchpail'
    This should help us to distinguish lunchpail-provided internal s3 from application-provided services named "s3". Though we already had the run name in the service name, this should hopefully further help us to distinguish *our* service.
    
    Also fixes test7f/preinit.sh, which didn't handle the case where the `rcloneremotetest` config had changed.
    Also refactors the workstealer chart to place some of the s3 bits under a subdirectory.
    
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- tests/tests/test7f/preinit.sh
+++ tests/tests/test7f/preinit.sh
@@ -1,13 +1,11 @@
-if ! grep rcloneremotetest ~/.config/rclone/rclone.conf
-then
-    cat <<'EOF' >> ~/.config/rclone/rclone.conf 
+rclone config delete rcloneremotetest
+cat <<'EOF' >> ~/.config/rclone/rclone.conf 
 [rcloneremotetest]
 type = s3
 provider = Other
 env_auth = false
-endpoint = http://$TEST_RUN-s3.test7f.svc.cluster.local:$TEST_PORT
+endpoint = http://$TEST_RUN-lunchpail-s3.test7f.svc.cluster.local:$TEST_PORT
 access_key_id = $TEST_ACCESSKEY
 secret_access_key = $TEST_SECRETKEY
 acl = public-read
 EOF
-fi
