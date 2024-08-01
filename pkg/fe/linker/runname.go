c5ca72701abf4ed96ecb702e166b924e5406f531
aavarghese <avarghese@us.ibm.com>
2024-08-01 13:15:45 -0400

    feat: add username to runname
    Signed-off-by: aavarghese <avarghese@us.ibm.com>

--- pkg/fe/linker/runname.go
+++ pkg/fe/linker/runname.go
@@ -1,9 +1,11 @@
 package linker
 
 import (
+	"os/user"
+	"strings"
+
 	"github.com/google/uuid"
 	"lunchpail.io/pkg/util"
-	"strings"
 )
 
 func GenerateRunName(appname string) (string, error) {
@@ -19,7 +21,13 @@ func GenerateRunName(appname string) (string, error) {
 			ids = ids[:idx]
 		}
 
-		runname = util.Truncate(runname+"-"+ids, 53)
+		currentUser, err := user.Current()
+		if err != nil {
+			return "", err
+		}
+		username := util.Truncate(currentUser.Username, 4)
+
+		runname = util.Truncate(runname+"-"+username+"-"+ids, 53)
 	}
 
 	return runname, nil
