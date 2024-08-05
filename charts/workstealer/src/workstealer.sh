bd374293fd8977ce1051dfa2f075960468aac2fb
Nick Mitchell <nickm@us.ibm.com>
2024-08-05 14:57:08 -0400

    feat: tee workstealer output to queue logs/workstealer.txt
    This should help with log presevation and (primary goal) provide access to logs and queue stats without floating ip access to virtual machines.
    
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- charts/workstealer/src/workstealer.sh
+++ charts/workstealer/src/workstealer.sh
@@ -1,31 +1,8 @@
 #!/usr/bin/env bash
 
-SCRIPTDIR=$(cd $(dirname "$0") && pwd)
-
-LOCAL_QUEUE_ROOT=$(mktemp -d /tmp/localqueue.XXXXXXXX)
-export QUEUE=$LOCAL_QUEUE_ROOT/$LUNCHPAIL_QUEUE_PATH
-
-remote=s3:/$LUNCHPAIL_QUEUE_PATH
-
-S3_ENDPOINT=http://localhost:9000
-
-if [[ -z "$MINIO_ENABLED" ]]
-then S3_ENDPOINT=$lunchpail_queue_endpoint
-fi
-
-# the rclone.conf file
-config=~/.config/rclone/rclone.conf
-mkdir -p $(dirname $config)
-cat <<EOF > $config
-[s3]
-type = s3
-provider = Other
-env_auth = false
-endpoint = $S3_ENDPOINT
-access_key_id = $lunchpail_queue_accessKeyID
-secret_access_key = $lunchpail_queue_secretAccessKey
-acl = public-read
-EOF
+#
+# Note: this script expects to be launched by ./launcher.sh
+#
 
 if [[ -n "$MINIO_ENABLED" ]]
 then
