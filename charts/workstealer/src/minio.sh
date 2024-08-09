f3860cfcaf5bb8b5316b73e8274a7c621c3d3b60
Nick Mitchell <nickm@us.ibm.com>
2024-08-09 12:04:48 -0400

    feat: update workstealer image to use alpine rather than debian slim
    saves 64 megabytes, plus this should help with unifying the workstealer into a singular lunchpail image
    
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- charts/workstealer/src/minio.sh
+++ charts/workstealer/src/minio.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 DATA_DIR=./data
 
 export MINIO_ROOT_USER=$INTERNAL_S3_ACCESS_KEY
 export MINIO_ROOT_PASSWORD=$INTERNAL_S3_SECRET_KEY
 
-if [[ ! -d $DATA_DIR ]]
+if [ ! -d "$DATA_DIR" ]
 then mkdir $DATA_DIR
 fi
 
