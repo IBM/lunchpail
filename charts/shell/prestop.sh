7d1b24885a8bed6859d991dd45c464222d07803a
Nick Mitchell <nickm@us.ibm.com>
2024-08-12 18:57:49 -0400

    chore: clean out prestop shell script in charts/shell
    with `lunchpail qdone` we no longer need a shell script with volume/volumeMount/configMap.
    
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- charts/shell/prestop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "DEBUG prestop starting"
-
-echo "DEBUG prestop touching donefile"
-lunchpail qdone
-echo "DEBUG prestop touching donefile: done"
-
-echo "INFO Done with my part of the job"
