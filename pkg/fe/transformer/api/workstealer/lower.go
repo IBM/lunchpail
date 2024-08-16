94c50dd5719c444df83c529b24c405af261033c0
Nick Mitchell <nickm@us.ibm.com>
2024-08-16 12:40:53 -0400

    refactor: update workstealer to transpile to shell (remove charts/workstealer)
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- pkg/fe/transformer/api/workstealer/lower.go
+++ pkg/fe/transformer/api/workstealer/lower.go
@@ -1,53 +1,31 @@
 package workstealer
 
 import (
-	"fmt"
-	"os"
-	"strings"
-
 	"lunchpail.io/pkg/compilation"
 	"lunchpail.io/pkg/fe/linker/queue"
-	"lunchpail.io/pkg/fe/transformer/api"
+	"lunchpail.io/pkg/fe/transformer/api/shell"
 	"lunchpail.io/pkg/ir/hlir"
 	"lunchpail.io/pkg/ir/llir"
 	"lunchpail.io/pkg/lunchpail"
 )
 
 func Lower(compilationName, runname, namespace string, app hlir.Application, queueSpec queue.Spec, opts compilation.Options, verbose bool) (llir.Component, error) {
-	templatePath, err := api.Stage(template, templateFile)
+	app, err := transpile(runname, queueSpec)
 	if err != nil {
 		return llir.Component{}, err
-	} else if verbose {
-		fmt.Fprintf(os.Stderr, "Shell stage %s\n", templatePath)
-	} else {
-		defer os.RemoveAll(templatePath)
 	}
 
-	values := []string{
-		"name=" + runname,
-		"partOf=" + compilationName,
-		"namespace.user=" + namespace,
-		"lunchpail=lunchpail",
-		"mcad.enabled=false",
-		"image.registry=" + lunchpail.ImageRegistry,
-		"image.repo=" + lunchpail.ImageRepo,
-		"image.version=" + lunchpail.Version(),
-		fmt.Sprintf("internalS3.enabled=%v", queueSpec.Auto),
-		fmt.Sprintf("internalS3.port=%d", queueSpec.Port),
-		"internalS3.accessKey=lunchpail", // TODO externalize
-		"internalS3.secretKey=lunchpail", // TODO externalize
-		"taskqueue.dataset=" + queueSpec.Name,
-		"taskqueue.endpoint=" + queueSpec.Endpoint,
-		"taskqueue.bucket=" + queueSpec.Bucket,
-		"taskqueue.accessKey=" + queueSpec.AccessKey,
-		"taskqueue.secretKey=" + queueSpec.SecretKey,
-		"taskqueue.prefixPath=" + api.QueuePrefixPath(queueSpec, runname),
-		"sleep_before_exit=" + os.Getenv("LUNCHPAIL_SLEEP_BEFORE_EXIT"),
-	}
+	component, err := shell.LowerAsComponent(
+		compilationName,
+		runname,
+		namespace,
+		app,
+		queueSpec,
+		"", // no service account needed
+		opts,
+		verbose,
+		lunchpail.WorkStealerComponent,
+	)
 
-	if verbose {
-		fmt.Fprintf(os.Stderr, "Workstealer values\n%s\n", "\n  -"+strings.Join(values, "\n  - "))
-	}
-
-	return api.GenerateComponent(runname, namespace, templatePath, values, verbose, lunchpail.WorkStealerComponent)
+	return component, err
 }
