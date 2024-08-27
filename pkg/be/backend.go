0d7ea872f6af3691de26621a1e761667e60eb6c4
Nick Mitchell <nickm@us.ibm.com>
2024-08-27 10:46:28 -0400

    refactor: split out be.New logic into separate file
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- pkg/be/backend.go
+++ pkg/be/backend.go
@@ -1,13 +1,8 @@
 package be
 
 import (
-	"fmt"
-
-	"lunchpail.io/pkg/be/ibmcloud"
-	"lunchpail.io/pkg/be/kubernetes"
 	"lunchpail.io/pkg/be/platform"
 	"lunchpail.io/pkg/be/runs"
-	"lunchpail.io/pkg/compilation"
 	"lunchpail.io/pkg/ir"
 )
 
@@ -26,27 +21,4 @@ type Backend interface {
 
 	// List deployed runs
 	ListRuns(appName, namespace string) ([]runs.Run, error)
-}
-
-func New(backend platform.Platform, aopts compilation.Options) (Backend, error) {
-	var be Backend
-
-	switch backend {
-	case platform.Kubernetes:
-		be = kubernetes.Backend{}
-	case platform.IBMCloud:
-		if ibm, err := ibmcloud.New(aopts); err != nil {
-			return nil, err
-		} else {
-			be = ibm
-		}
-	default:
-		return nil, fmt.Errorf("Unsupported backend %v", backend)
-	}
-
-	if err := be.Ok(); err != nil {
-		return nil, err
-	}
-
-	return be, nil
 }
