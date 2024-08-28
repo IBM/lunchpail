067a1fcd3d34289dc68a29ecc17e741ecc235e3c
Nick Mitchell <nickm@us.ibm.com>
2024-08-27 21:09:35 -0400

    fix: trim more from the image builds
    - eliminates the superfluous use of charmbracelet from pkg/be
    - adds go:build tag to info command
    
    19.2Mi -> 18.1Mi for the image
    9.8Mi -> 8.8Mi for the binary build (in the image)
    
    Overall, down from 60Mi for the binary build (in the image)
    
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- pkg/be/runs/pretty.go
+++ pkg/be/runs/pretty.go
@@ -5,17 +5,15 @@ import (
 	"strings"
 	"time"
 
-	"github.com/charmbracelet/lipgloss"
 	"github.com/dustin/go-humanize"
 )
 
 func Pretty(runs []Run) string {
 	names := []string{}
 	now := time.Now()
-	dim := lipgloss.NewStyle().Faint(true)
 
 	for _, run := range runs {
-		names = append(names, fmt.Sprintf("%s %s", run.Name, dim.Render(humanize.RelTime(run.CreationTimestamp, now, "ago", "from now"))))
+		names = append(names, fmt.Sprintf("%s %s", run.Name, humanize.RelTime(run.CreationTimestamp, now, "ago", "from now")))
 	}
 
 	return strings.Join(names, "\n")
