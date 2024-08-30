ac35dff6a85256c9109e86772b801634e9c02cd8
Nick Mitchell <nickm@us.ibm.com>
2024-08-30 11:47:53 -0400

    fix: remove superfluous ir.Linked struct altogether
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- pkg/boot/up.go
+++ pkg/boot/up.go
@@ -14,26 +14,26 @@ import (
 type UpOptions = fe.CompileOptions
 
 func upDown(backend be.Backend, opts UpOptions, isUp bool) error {
-	linked, err := fe.PrepareForRun(opts)
+	ir, copts, err := fe.PrepareForRun(opts)
 	if err != nil {
 		return err
 	}
 
-	cliOptions := options.CliOptions{
-		CreateNamespace: linked.Options.CreateNamespace,
-		ImagePullSecret: linked.Options.ImagePullSecret,
+	cliOpts := options.CliOptions{
+		CreateNamespace: copts.CreateNamespace,
+		ImagePullSecret: copts.ImagePullSecret,
 	}
 
 	if opts.DryRun {
-		fmt.Printf(backend.DryRun(linked.Ir, cliOptions, opts.Verbose))
+		fmt.Printf(backend.DryRun(ir, cliOpts, opts.Verbose))
 		return nil
 	} else if isUp {
-		if err := backend.Up(linked, cliOptions, opts.Verbose); err != nil {
+		if err := backend.Up(ir, copts, cliOpts, opts.Verbose); err != nil {
 			return err
 		} else if opts.Watch {
-			return status.UI(linked.Ir.RunName, backend, status.Options{Watch: true, Verbose: opts.Verbose, Summary: false, Nloglines: 500, IntervalSeconds: 5})
+			return status.UI(ir.RunName, backend, status.Options{Watch: true, Verbose: opts.Verbose, Summary: false, Nloglines: 500, IntervalSeconds: 5})
 		}
-	} else if err := backend.Down(linked, cliOptions, opts.Verbose); err != nil {
+	} else if err := backend.Down(ir, copts, cliOpts, opts.Verbose); err != nil {
 		return err
 	}
 
