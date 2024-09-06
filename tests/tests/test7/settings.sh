d7ea2f52c1f9c27391a7e62a36ad84c0253ba252
Nick Mitchell <nickm@us.ibm.com>
2024-09-06 12:35:41 -0400

    test: update test7 validator to expect 6 output files
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- tests/tests/test7/settings.sh
+++ tests/tests/test7/settings.sh
@@ -4,3 +4,4 @@ api=workqueue
 # task.1,task.3,task.5 <-- 3 tasks per iter
 
 expected=("Processing 6 task.1.txt" "Processing 6 task.3.txt" "Processing 6 task.5.txt" "Processing 6 task.2.txt" "Processing 6 task.4.txt" "Processing 6 task.6.txt")
+NUM_DESIRED_OUTPUTS=6
