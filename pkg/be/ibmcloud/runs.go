ac6c05274c45a6053a9154ba56cf70cef10d5b57
aavarghese <avarghese@us.ibm.com>
2024-09-11 10:12:52 -0400

    fix: ibmcloud be should delete cloud resources for multiple runs
    Signed-off-by: aavarghese <avarghese@us.ibm.com>

--- pkg/be/ibmcloud/runs.go
+++ pkg/be/ibmcloud/runs.go
@@ -1,8 +1,35 @@
 package ibmcloud
 
-import "lunchpail.io/pkg/be/runs"
+import (
+	"fmt"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/IBM/vpc-go-sdk/vpcv1"
+	"lunchpail.io/pkg/be/runs"
+	"lunchpail.io/pkg/compilation"
+)
 
 func (backend Backend) ListRuns() ([]runs.Run, error) {
-	// TODO
-	return []runs.Run{}, nil
+	appName := compilation.Name()
+	resourceGroupID := backend.config.ResourceGroup.GUID
+	vpcRuns := []runs.Run{}
+
+	vpcs, response, err := backend.vpcService.ListVpcs(&vpcv1.ListVpcsOptions{
+		ResourceGroupID: &resourceGroupID,
+		Headers:         map[string]string{"AppName": appName},
+	})
+	if err != nil {
+		return vpcRuns, fmt.Errorf("failed to get vpcs: %v and the response is: %s", err, response)
+	}
+
+	for _, vpc := range vpcs.Vpcs {
+		if strings.HasPrefix(*vpc.Name, appName) { //Contains instead of prefix?
+			vpcRuns = append(vpcRuns, runs.Run{Name: *vpc.Name, CreationTimestamp: time.Time(*vpc.CreatedAt)})
+		}
+	}
+
+	sort.Slice(vpcRuns, func(i, j int) bool { return vpcRuns[i].CreationTimestamp.Before(vpcRuns[j].CreationTimestamp) })
+	return vpcRuns, nil
 }
