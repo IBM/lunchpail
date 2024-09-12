ca64daebd61c4efa7f94ad371bc6ef0e5b0fb45c
Nick Mitchell <nickm@us.ibm.com>
2024-09-12 10:00:08 -0400

    refactor: pull out common parts of qstat streaming
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- pkg/be/kubernetes/qstat.go
+++ pkg/be/kubernetes/qstat.go
@@ -1,12 +1,9 @@
 package kubernetes
 
 import (
-	"bufio"
 	"context"
 	"fmt"
 	"os"
-	"slices"
-	"strconv"
 	"strings"
 	"time"
 
@@ -16,7 +13,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"lunchpail.io/pkg/be/events/qstat"
-	"lunchpail.io/pkg/fe/transformer/api"
+	streamerCommon "lunchpail.io/pkg/be/streamer"
 )
 
 func (streamer Streamer) streamModel(runname string, follow bool, tail int64, quiet bool, c chan qstat.Model) error {
@@ -53,106 +50,8 @@ func (streamer Streamer) streamModel(runname string, follow bool, tail int64, qu
 			return err
 		}
 	}
-	buffer := bufio.NewReader(stream)
 
-	model := qstat.Model{}
-	for {
-		line, err := buffer.ReadString('\n')
-		if err != nil { // == io.EOF {
-			break
-		}
-
-		if !strings.HasPrefix(line, "lunchpail.io") {
-			continue
-		}
-
-		fields := strings.Fields(line)
-		if len(fields) == 0 {
-			continue
-		}
-
-		marker := fields[1]
-		if marker == "---" && model.Valid {
-			c <- model
-			model = qstat.Model{Valid: true}
-			continue
-		} else if len(fields) >= 3 {
-			count, err := strconv.Atoi(fields[2])
-			if err != nil {
-				continue
-			}
-
-			if marker == "unassigned" {
-				model.Valid = true
-				model.Unassigned = count
-				model.Timestamp = strings.Join(fields[4:], " ")
-			} else if marker == "assigned" {
-				model.Assigned = count
-			} else if marker == "processing" {
-				model.Processing = count
-			} else if marker == "done" {
-				count2, err := strconv.Atoi(fields[3])
-				if err != nil {
-					continue
-				}
-
-				model.Success = count
-				model.Failure = count2
-			} else if marker == "liveworker" || marker == "deadworker" {
-				count2, err := strconv.Atoi(fields[3])
-				if err != nil {
-					continue
-				}
-				count3, err := strconv.Atoi(fields[4])
-				if err != nil {
-					continue
-				}
-				count4, err := strconv.Atoi(fields[5])
-				if err != nil {
-					continue
-				}
-
-				// The workstealer labels workers with
-				// the suffix of the queue path that
-				// we gave it via
-				// api.QueuePrefixPathForWorker(). See
-				// e.g. `assignedWorkPattern`. Here,
-				// we reverse that to extract the
-				// worker and pool name.
-				poolName, workerName, err := api.ExtractNamesFromSubPathForWorker(fields[6])
-				if err != nil {
-					continue
-				}
-
-				worker := qstat.Worker{
-					Name: workerName, Inbox: count, Processing: count2, Outbox: count3, Errorbox: count4,
-				}
-
-				pidx := slices.IndexFunc(model.Pools, func(pool qstat.Pool) bool { return pool.Name == poolName })
-				var pool qstat.Pool
-				if pidx < 0 {
-					// new pool
-					pool = qstat.Pool{Name: poolName}
-				} else {
-					pool = model.Pools[pidx]
-				}
-
-				if marker == "liveworker" {
-					pool.LiveWorkers = append(pool.LiveWorkers, worker)
-				} else {
-					pool.DeadWorkers = append(pool.DeadWorkers, worker)
-				}
-
-				if pidx < 0 {
-					model.Pools = append(model.Pools, pool)
-				} else {
-					model.Pools = slices.Concat(model.Pools[:pidx], []qstat.Pool{pool}, model.Pools[pidx+1:])
-				}
-			}
-		}
-	}
-
-	return nil
+	return streamerCommon.QstatFromStream(stream, c)
 }
 
 func (streamer Streamer) QueueStats(runname string, opts qstat.Options) (chan qstat.Model, *errgroup.Group, error) {
---
+++ pkg/be/streamer/qstat.go
@@ -0,0 +1,114 @@
+package streamer
+
+import (
+	"bufio"
+	"io"
+	"slices"
+	"strconv"
+	"strings"
+
+	"lunchpail.io/pkg/be/events/qstat"
+	"lunchpail.io/pkg/fe/transformer/api"
+)
+
+func QstatFromStream(stream io.ReadCloser, c chan qstat.Model) error {
+	buffer := bufio.NewReader(stream)
+	model := qstat.Model{}
+	for {
+		line, err := buffer.ReadString('\n')
+		if err != nil { // == io.EOF {
+			break
+		}
+
+		if !strings.HasPrefix(line, "lunchpail.io") {
+			continue
+		}
+
+		fields := strings.Fields(line)
+		if len(fields) == 0 {
+			continue
+		}
+
+		marker := fields[1]
+		if marker == "---" && model.Valid {
+			c <- model
+			model = qstat.Model{Valid: true}
+			continue
+		} else if len(fields) >= 3 {
+			count, err := strconv.Atoi(fields[2])
+			if err != nil {
+				continue
+			}
+
+			if marker == "unassigned" {
+				model.Valid = true
+				model.Unassigned = count
+				model.Timestamp = strings.Join(fields[4:], " ")
+			} else if marker == "assigned" {
+				model.Assigned = count
+			} else if marker == "processing" {
+				model.Processing = count
+			} else if marker == "done" {
+				count2, err := strconv.Atoi(fields[3])
+				if err != nil {
+					continue
+				}
+
+				model.Success = count
+				model.Failure = count2
+			} else if marker == "liveworker" || marker == "deadworker" {
+				count2, err := strconv.Atoi(fields[3])
+				if err != nil {
+					continue
+				}
+				count3, err := strconv.Atoi(fields[4])
+				if err != nil {
+					continue
+				}
+				count4, err := strconv.Atoi(fields[5])
+				if err != nil {
+					continue
+				}
+
+				// The workstealer labels workers with
+				// the suffix of the queue path that
+				// we gave it via
+				// api.QueuePrefixPathForWorker(). See
+				// e.g. `assignedWorkPattern`. Here,
+				// we reverse that to extract the
+				// worker and pool name.
+				poolName, workerName, err := api.ExtractNamesFromSubPathForWorker(fields[6])
+				if err != nil {
+					continue
+				}
+
+				worker := qstat.Worker{
+					Name: workerName, Inbox: count, Processing: count2, Outbox: count3, Errorbox: count4,
+				}
+
+				pidx := slices.IndexFunc(model.Pools, func(pool qstat.Pool) bool { return pool.Name == poolName })
+				var pool qstat.Pool
+				if pidx < 0 {
+					// new pool
+					pool = qstat.Pool{Name: poolName}
+				} else {
+					pool = model.Pools[pidx]
+				}
+
+				if marker == "liveworker" {
+					pool.LiveWorkers = append(pool.LiveWorkers, worker)
+				} else {
+					pool.DeadWorkers = append(pool.DeadWorkers, worker)
+				}
+
+				if pidx < 0 {
+					model.Pools = append(model.Pools, pool)
+				} else {
+					model.Pools = slices.Concat(model.Pools[:pidx], []qstat.Pool{pool}, model.Pools[pidx+1:])
+				}
+			}
+		}
+	}
+
+	return nil
+}
