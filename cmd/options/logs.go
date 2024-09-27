5349f9d0f3b2480f73d51a78e4f968e8bb14756e
Nick Mitchell <nickm@us.ibm.com>
2024-09-27 09:08:29 -0400

    feat: change --debug short form from -d to -g
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- cmd/options/logs.go
+++ cmd/options/logs.go
@@ -14,7 +14,7 @@ func AddLogOptionsTo(cmd *cobra.Command, opts *build.Options) *build.LogOptions 
 		opts.Log = &build.LogOptions{}
 	}
 
-	cmd.Flags().BoolVarP(&opts.Log.Debug, "debug", "d", opts.Log.Debug, "Debug output")
+	cmd.Flags().BoolVarP(&opts.Log.Debug, "debug", "g", opts.Log.Debug, "Debug output")
 	cmd.Flags().BoolVarP(&opts.Log.Verbose, "verbose", "v", opts.Log.Verbose, "Verbose output")
 	return opts.Log
 }
