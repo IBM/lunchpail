4aa3ed6a017d79f4dd5b94459a343764007b22f9
Nick Mitchell <nickm@us.ibm.com>
2024-10-01 14:49:20 -0400

    fix: queue ls may not report errors
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- pkg/runtime/queue/ls.go
+++ pkg/runtime/queue/ls.go
@@ -8,22 +8,28 @@ import (
 	"lunchpail.io/pkg/be"
 )
 
-func Ls(ctx context.Context, backend be.Backend, runname, path string) (chan string, error) {
+func Ls(ctx context.Context, backend be.Backend, runname, path string) (chan string, chan error, error) {
 	c, stop, err := NewS3ClientForRun(ctx, backend, runname)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	files := make(chan string)
+	errors := make(chan error)
 	prefix := filepath.Join(c.Paths.Prefix, path)
 
 	go func() {
 		defer stop()
 		defer close(files)
+		defer close(errors)
 		for o := range c.ListObjects(c.Paths.Bucket, prefix, true) {
-			files <- strings.Replace(o.Key, prefix+"/", "", 1)
+			if o.Err != nil {
+				errors <- o.Err
+			} else {
+				files <- strings.Replace(o.Key, prefix+"/", "", 1)
+			}
 		}
 	}()
 
-	return files, nil
+	return files, errors, nil
 }
