fd9c522659664fee94d9c528e890672fddc40983
Nick Mitchell <nickm@us.ibm.com>
2024-10-01 10:05:04 -0400

    fix: improve validation when file is expected by dir is given
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- pkg/fe/builder/overlay/helmignore.go
+++ pkg/fe/builder/overlay/helmignore.go
@@ -11,7 +11,11 @@ import (
 // If the app has a .helmignore file, append it to the one in the template
 func handleHelmIgnore(templatePath string, verbose bool) error {
 	appHelmignore := filepath.Join(appdir(templatePath), ".helmignore")
-	if _, err := os.Stat(appHelmignore); err == nil {
+	if f, err := os.Stat(appHelmignore); err == nil {
+		if f.IsDir() {
+			return fmt.Errorf(".helmignore should be a file, not a directory")
+		}
+
 		fmt.Fprintf(os.Stderr, "Including application helmignore\n")
 		templateHelmignore := filepath.Join(templatePath, ".helmignore")
 		if err := util.AppendFile(templateHelmignore, appHelmignore); err != nil {
