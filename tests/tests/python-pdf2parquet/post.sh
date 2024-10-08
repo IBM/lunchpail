5a4e3a004f071bfef82b577c84ea545beb52d6ee
Nick Mitchell <nickm@us.ibm.com>
2024-10-07 20:24:39 -0400

    test: add python-pdf2parquet test
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

---
+++ tests/tests/python-pdf2parquet/post.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+DATA="$TEST_PATH"/pail/test-data
+
+for i in "$DATA"/input/*
+do
+    b=$(basename $i)
+    if [[ "$b" =~ "output" ]]
+    then continue
+    fi
+
+    ext=${b##*.}
+    bb=${b%%.*}
+    actual="$(dirname $i)"/"$bb".output.$ext
+    expected="$DATA"/expected/$bb.parquet.gz
+
+    while true
+    do
+        if [ -f $actual ]
+        then echo "✅ PASS found local task output file=$actual test=$TEST_NAME" && break
+        else echo "Still waiting for local task output file=$actual test=$TEST_NAME" && sleep 1
+        fi
+    done
+
+    actual_sha256=$(cat "$actual" | sha256sum)
+    expected_sha256=$(gunzip -c "$expected" | sha256sum)
+
+    if [ "$actual_sha256" = "$expected_sha256" ]
+    then echo "✅ PASS the output file is valid file=$actual test=$TEST_NAME"
+    else echo "❌ FAIL mismatched sha256 on output file file=$actual expected=$expected actual_sha256=$actual_sha256 expected_sha256=$expected_sha256 test=$TEST_NAME" && exit 1
+    fi
+
+    rm -f "$actual"
+done
