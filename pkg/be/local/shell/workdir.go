a1a866fefccaa856ebaf509c77970d983a9390d1
aavarghese <avarghese@us.ibm.com>
2024-10-11 16:29:39 -0400

    feat: use lunchpail needs to install minio
    Signed-off-by: aavarghese <avarghese@us.ibm.com>

--- pkg/be/local/shell/workdir.go
+++ pkg/be/local/shell/workdir.go
@@ -2,16 +2,13 @@ package shell
 
 import (
 	base64 "encoding/base64"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
-	"os/exec"
 	"path/filepath"
 
 	"lunchpail.io/pkg/ir/hlir"
 	"lunchpail.io/pkg/ir/llir"
-	"lunchpail.io/pkg/lunchpail"
 )
 
 func PrepareWorkdirForComponent(c llir.ShellComponent, verbose bool) (string, string, error) {
@@ -32,35 +29,11 @@ func PrepareWorkdirForComponent(c llir.ShellComponent, verbose bool) (string, st
 
 	command := c.Application.Spec.Command
 
-	// hmm, hacky attempts to get intrinsic prereqs
-	switch c.C() {
-	case lunchpail.MinioComponent:
-		if err := ensureMinio(); err != nil {
-			return "", "", err
-		}
-	}
-
 	return workdir, command, nil
 }
 
 func saveCodeToWorkdir(workdir string, code hlir.Code) error {
 	return os.WriteFile(filepath.Join(workdir, code.Name), []byte(code.Source), 0700)
-}
-
-func ensureMinio() error {
-	if err := setenvForMinio(); err != nil {
-		return err
-	}
-
-	if _, err := exec.LookPath("minio"); err != nil {
-		if errors.Is(err, exec.ErrNotFound) {
-			return installMinio()
-		} else if err != nil {
-			return err
-		}
-	}
-
-	return nil
 }
 
 func writeBlobsToWorkdir(c llir.ShellComponent, workdir string, verbose bool) error {
--- pkg/runtime/needs/minio.go
+++ pkg/runtime/needs/minio.go
@@ -9,7 +9,6 @@ import (
 func InstallMinio(ctx context.Context, version string, opts Options) error {
 	if _, err := exec.LookPath("minio"); err != nil {
 		if errors.Is(err, exec.ErrNotFound) {
-			//Todo: use context?
 			return installMinio(ctx, version, opts.Verbose)
 		}
 		return err
