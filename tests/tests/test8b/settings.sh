5e42b3477614f705a485baa98783f6751dc6e37a
Nick Mitchell <nickm@us.ibm.com>
2024-10-14 13:40:27 -0400

    test: test8/8b shouldn't wait for full completion
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- tests/tests/test8b/settings.sh
+++ tests/tests/test8b/settings.sh
@@ -3,7 +3,10 @@ app=https://github.com/IBM/lunchpail-openroad-max-utilization.git
 branch=v0.3.0
 deployname=lunchpail-openroad-max-utilization
 
-# we kill the process before it is finishes
+# don't inspect output for validity
 NUM_DESIRED_OUTPUTS=0
+
+# we kill the process before it is finished
+NO_WAIT_FOR_COMPLETION=1
 
 expected=("Running experiment")
