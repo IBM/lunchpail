9a158df0d6a4ffc8df956ac540cdc58bea65c568
Nick Mitchell <nickm@us.ibm.com>
2024-10-23 12:14:26 -0400

    test: add1 pipeline tests
    This adds `add1 | add1 | add1` pipeline tests.
    
    This adds an `add1` command... until we have support for a `run -e` to allow for ad-hoc command line worker functions, i’ll add an add1 builtin (alongside the cat builtin) to help with tests/demos
    
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

---
+++ pkg/runtime/builtins/add1.go
@@ -0,0 +1,21 @@
+package builtins
+
+import (
+	"lunchpail.io/pkg/ir/hlir"
+)
+
+func Add1App() hlir.HLIR {
+	app := hlir.NewApplication("add1")
+	app.Spec.Role = "worker"
+	app.Spec.Command = "./main.sh"
+	app.Spec.Image = "docker.io/alpine:3"
+	app.Spec.Code = []hlir.Code{
+		hlir.Code{Name: "main.sh", Source: `#!/bin/sh
+printf '%d' $((1+$(cat $1))) > $2`},
+	}
+
+	return hlir.HLIR{
+		Applications: []hlir.Application{app},
+		WorkerPools:  []hlir.WorkerPool{hlir.NewPool("add1", 1)},
+	}
+}
