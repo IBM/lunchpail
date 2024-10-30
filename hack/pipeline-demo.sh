2b17209efbac578c4eae497cc724b417c56afa31
Nick Mitchell <nickm@us.ibm.com>
2024-10-30 14:33:37 -0400

    feat: add hack/pipeline-demo.sh
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

---
+++ hack/pipeline-demo.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+SCRIPTDIR=$(cd $(dirname "$0") && pwd)
+TOP="$SCRIPTDIR"/../..
+
+lp=./lunchpail
+if [ ! -e $lp ]
+then "$TOP"/hack/setup/cli.sh $lp
+fi
+
+IN1=$(mktemp)
+echo "1" > $IN1
+trap "rm -f $IN1 $fail $add1b $add1c $add1d" EXIT
+
+export LUNCHPAIL_NAME="pipeline-demo"
+export LUNCHPAIL_TARGET=${LUNCHPAIL_TARGET:-local}
+
+stepo=./pipeline-demo
+if [ ! -e $stepo ]
+then ./lunchpail build -e 'echo "hi from step $LUNCHPAIL_STEP"; sleep 2' -o $stepo
+fi
+
+step="$stepo up --verbose=${VERBOSE:-false}"
+
+echo "Launching pipeline"
+$step <(echo 1) <(echo 2) <(echo 3) <(echo 4) <(echo 5) <(echo 6) <(echo 7) <(echo 8) <(echo 9) <(echo 10) | $step | $step | $step
