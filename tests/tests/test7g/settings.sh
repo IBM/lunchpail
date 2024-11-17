ce6f565ecb69a88ca9e0714711764381bea9acd0
Nick Mitchell <nickm@us.ibm.com>
2024-11-17 13:13:20 -0500

    test: remove role: worker from test specs as it is now the default behavior
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- tests/tests/test7g/settings.sh
+++
@@ -1,8 +0,0 @@
-api=workqueue
-
-# /queue/0,1 <-- 2 workers
-# task.1,task.3,task.5 <-- 3 tasks per iter
-
-# "tasky" and 3333 come from a values override in the workdispatcher.yaml
-
-expected=('Processing tasky3333.1.txt 99999999 88888888' 'Processing tasky3333.3.txt 99999999 88888888' 'Processing tasky3333.5.txt 99999999 88888888' 'Processing tasky3333.2.txt 99999999 88888888' 'Processing tasky3333.4.txt 99999999 88888888' 'Processing tasky3333.6.txt 99999999 88888888')
