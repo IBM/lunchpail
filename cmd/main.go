d58d1890426e54eb45790b1afe554f14b9af00a6
Nick Mitchell <nickm@us.ibm.com>
2024-11-18 12:45:48 -0500

    fix: leverage github.com/uber-go/automaxprocs to have go recognize cgroups cpu limits
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "lunchpail.io/cmd/subcommands"
+import (
+	"go.uber.org/automaxprocs/maxprocs"
+
+	"lunchpail.io/cmd/subcommands"
+)
 
 func main() {
+	// Automatically set GOMAXPROCS to match Linux container CPU quota.
+	// https://github.com/uber-go/automaxprocs
+	maxprocs.Set()
+
 	subcommands.Execute()
 }
