d6014bc0c499700cb258cd20fac5d9356d207dc7
Nick Mitchell <nickm@us.ibm.com>
2024-11-27 17:22:24 -0500

    test: update python-language-doc-chunk to avoid yaml
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- tests/tests/python-language-doc-chunk/settings.sh
+++ tests/tests/python-language-doc-chunk/settings.sh
@@ -3,4 +3,7 @@ api=workqueue
 expected=("Transforming one table with 1 rows" "Done with nfiles=1 nrows=88")
 NUM_DESIRED_OUTPUTS=0
 
+# the default is --yaml. we don't want that
+source_from=" "
+
 up_args='<(gunzip -c "$TEST_PATH"/pail/test-data/input/test1.parquet.gz)'
