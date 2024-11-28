8309c469f04782868fcf87ec57423b27f40b42ce
Nick Mitchell <nickm@us.ibm.com>
2024-11-27 19:09:49 -0500

    test: update python-language-text-encoder to avoid yaml
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- tests/tests/python-language-text-encoder/settings.sh
+++ tests/tests/python-language-text-encoder/settings.sh
@@ -3,4 +3,7 @@ api=workqueue
 expected=("Done with nfiles=1 nrows=2")
 NUM_DESIRED_OUTPUTS=0
 
+# the default is --yaml. we don't want that
+source_from=" "
+
 up_args='"$TEST_PATH"/pail/test-data/input/test1.parquet'
