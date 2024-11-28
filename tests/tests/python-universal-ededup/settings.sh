3a8c4ef8dcf22d3642388f74f51404e66aa46df5
Nick Mitchell <nickm@us.ibm.com>
2024-11-28 11:18:00 -0500

    test: port python-universal-ededup to avoid yaml
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- tests/tests/python-universal-ededup/settings.sh
+++ tests/tests/python-universal-ededup/settings.sh
@@ -3,4 +3,7 @@ api=workqueue
 expected=("input table has 5 rows and 38 columns" "output table has 3 rows and 39 columns" "output table has 3 rows and 39 columns")
 NUM_DESIRED_OUTPUTS=0
 
+# the default is --yaml. we don't want that
+source_from=" "
+
 up_args='<(gunzip -c "$TEST_PATH"/pail/test-data/input/sample1.parquet.gz)'
