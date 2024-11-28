bf4aa04f0c5cef3392d5d81b851952ca33cfc358
Nick Mitchell <nickm@us.ibm.com>
2024-11-28 14:12:10 -0500

    refactor: code cleanup to builder/overlay/source.go
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- pkg/fe/builder/overlay/source.go
+++ pkg/fe/builder/overlay/source.go
@@ -57,73 +57,69 @@ func applicationFromSource(appname, sourcePath, templatePath string, opts Option
 	srcPrefix := filepath.Join(sourcePath, "src")
 
 	err = filepath.WalkDir(sourcePath, func(path string, d fs.DirEntry, err error) error {
-		switch {
-		case d.IsDir():
+		if d.IsDir() {
 			// skip directories, except to remember which "mode" we are in
-		case filepath.Ext(path) == ".pdf" || filepath.Ext(path) == ".html" || filepath.Ext(path) == ".gz" || filepath.Ext(path) == ".zip" || filepath.Ext(path) == ".parquet":
-			// skip data files; TODO add support for .ignore
-		case path[len(path)-1] == '~':
-			// skip emacs temporary files
-		default:
-			if strings.HasPrefix(path, srcPrefix) {
-				// Handle src/ artifacts
-				source, err := readString(path)
-				if err != nil {
-					return err
-				}
-				spec.Code = append(spec.Code, hlir.Code{Name: d.Name(), Source: source})
+			return nil
+		}
 
-				switch d.Name() {
-				case "main.sh":
-					maybeCommand = "./main.sh"
-					maybeImage = "docker.io/alpine:3"
-				case "main.py":
-					maybeCommand = "python3 main.py"
-					maybeImage = "docker.io/python:3.12"
-				}
-				return nil
+		if strings.HasPrefix(path, srcPrefix) {
+			// Handle src/ artifacts
+			source, err := readString(path)
+			if err != nil {
+				return err
 			}
+			spec.Code = append(spec.Code, hlir.Code{Name: d.Name(), Source: source})
 
-			// Handle non-src artifacts
 			switch d.Name() {
-			case "version", "version.txt":
-				if appVersion, err = handleVersionFile(path); err != nil {
-					return err
-				}
-			case "requirements.txt":
-				req, err := readString(path)
-				if err != nil {
-					return err
-				}
-				spec.Needs = append(spec.Needs, hlir.Needs{Name: "python", Version: "latest", Requirements: req})
-			case "memory", "memory.txt":
-				mem, err := readString(path)
-				if err != nil {
-					return err
-				}
-				spec.MinMemory = mem
-			case "image":
-				image, err := readString(path)
-				if err != nil {
-					return err
-				}
-				spec.Image = image
-			case "command":
-				command, err := readString(path)
-				if err != nil {
-					return err
-				}
-				spec.Command = command
-			case "env.yaml":
-				if b, err := os.ReadFile(path); err != nil {
-					return err
-				} else if err := yaml.Unmarshal(b, &spec.Env); err != nil {
-					return fmt.Errorf("Error parsing env.yaml: %v", err)
-				}
-			default:
-				if opts.Verbose() {
-					fmt.Fprintln(os.Stderr, "Skipping application artifact", strings.Replace(path, sourcePath, "", 1))
-				}
+			case "main.sh":
+				maybeCommand = "./main.sh"
+				maybeImage = "docker.io/alpine:3"
+			case "main.py":
+				maybeCommand = "python3 main.py"
+				maybeImage = "docker.io/python:3.12"
+			}
+			return nil
+		}
+
+		// Handle non-src artifacts
+		switch d.Name() {
+		case "version", "version.txt":
+			if appVersion, err = handleVersionFile(path); err != nil {
+				return err
+			}
+		case "requirements.txt":
+			req, err := readString(path)
+			if err != nil {
+				return err
+			}
+			spec.Needs = append(spec.Needs, hlir.Needs{Name: "python", Version: "latest", Requirements: req})
+		case "memory", "memory.txt":
+			mem, err := readString(path)
+			if err != nil {
+				return err
+			}
+			spec.MinMemory = mem
+		case "image":
+			image, err := readString(path)
+			if err != nil {
+				return err
+			}
+			spec.Image = image
+		case "command":
+			command, err := readString(path)
+			if err != nil {
+				return err
+			}
+			spec.Command = command
+		case "env.yaml":
+			if b, err := os.ReadFile(path); err != nil {
+				return err
+			} else if err := yaml.Unmarshal(b, &spec.Env); err != nil {
+				return fmt.Errorf("Error parsing env.yaml: %v", err)
+			}
+		default:
+			if opts.Verbose() {
+				fmt.Fprintln(os.Stderr, "Skipping application artifact", strings.Replace(path, sourcePath, "", 1))
 			}
 		}
 
