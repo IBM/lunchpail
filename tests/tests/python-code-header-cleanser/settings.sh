0c728f8be787f979552d4abbe79e4ddff72748c6
Nick Mitchell <nickm@us.ibm.com>
2024-11-28 14:04:53 -0500

    test: port python-code-header-cleanser to avoid yaml
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- tests/tests/python-code-header-cleanser/settings.sh
+++ tests/tests/python-code-header-cleanser/settings.sh
@@ -3,4 +3,7 @@ api=workqueue
 expected=("input table has 10 rows" "output table has 10 rows")
 NUM_DESIRED_OUTPUTS=0
 
+# the default is --yaml. we don't want that
+source_from=" "
+
 up_args='<(gunzip -c "$TEST_PATH"/pail/test-data/input/test1.parquet.gz)'
