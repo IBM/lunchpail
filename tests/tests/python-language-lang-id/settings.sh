37c0e8031e4921b5f1974b8c9de1c44f10ca9f92
Nick Mitchell <nickm@us.ibm.com>
2024-11-28 10:37:51 -0500

    test: port python-language-lang-id to avoid yaml
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- tests/tests/python-language-lang-id/settings.sh
+++ tests/tests/python-language-lang-id/settings.sh
@@ -3,4 +3,7 @@ api=workqueue
 expected=("Transforming one table" "Done. Writing output to")
 NUM_DESIRED_OUTPUTS=0
 
+# the default is --yaml. we don't want that
+source_from=" "
+
 up_args='<(gunzip -c "$TEST_PATH"/pail/test-data/sm/input/test_01.parquet.gz) <(gunzip -c "$TEST_PATH"/pail/test-data/sm/input/test_02.parquet.gz) <(gunzip -c "$TEST_PATH"/pail/test-data/sm/input/test_03.parquet.gz)'
