3c4606f0fe3b0216af78d8cfc204b5c322333781
Nick Mitchell <nickm@us.ibm.com>
2024-11-28 11:07:20 -0500

    test: port python-language-pii-redactor to avoid yaml
    Also adds a memory constraint and removes app.yaml for lang-id (missing from prior PR)
    
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- tests/tests/python-language-pii-redactor/settings.sh
+++ tests/tests/python-language-pii-redactor/settings.sh
@@ -3,4 +3,7 @@ api=workqueue
 expected=("SequenceTagger predicts" "Done. Writing output to")
 NUM_DESIRED_OUTPUTS=0
 
+# the default is --yaml. we don't want that
+source_from=" "
+
 up_args='<(gunzip -c "$TEST_PATH"/pail/test-data/xs/1.parquet.gz)'
