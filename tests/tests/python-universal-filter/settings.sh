1176e92dfd3866ab1699d40241e7a44f74b480d3
Nick Mitchell <nickm@us.ibm.com>
2024-11-28 12:48:54 -0500

    test: port python-universal-filter to avoid yaml
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- tests/tests/python-universal-filter/settings.sh
+++ tests/tests/python-universal-filter/settings.sh
@@ -3,4 +3,7 @@ api=workqueue
 expected=("Done with docs_after_filter=100 columns_after_filter=25 bytes_after_filter=478602")
 NUM_DESIRED_OUTPUTS=0
 
+# the default is --yaml. we don't want that
+source_from=" "
+
 up_args='<(gunzip -c "$TEST_PATH"/pail/test-data/input/test1.parquet.gz)'
