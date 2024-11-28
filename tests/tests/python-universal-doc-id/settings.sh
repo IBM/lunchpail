54eb1c0d9d16ff3db342349314e49fbfac395a18
Nick Mitchell <nickm@us.ibm.com>
2024-11-28 11:14:31 -0500

    test: port python-universal-doc-id to avoid yaml
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- tests/tests/python-universal-doc-id/settings.sh
+++ tests/tests/python-universal-doc-id/settings.sh
@@ -3,4 +3,7 @@ api=workqueue
 expected=("input table has 5 rows" "output table has 5 rows")
 NUM_DESIRED_OUTPUTS=0
 
+# the default is --yaml. we don't want that
+source_from=" "
+
 up_args='<(gunzip -c "$TEST_PATH"/pail/test-data/input/sample1.parquet.gz)'
